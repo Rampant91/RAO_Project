7ab9c2f60143c19220c299e1fc885bb3115d7940
kastetbro <32741355+kastetbro@users.noreply.github.com>
2021-12-24 10:16:36 +0300

    migration fix

--- Models/DBRealization/Migrations/DataModel/20211221110132_DataModel_13.cs
+++ Models/DBRealization/Migrations/DataModel/20211221110132_DataModel_13.cs
@@ -13,6 +13,14 @@ namespace Models.DBRealization.Migrations.DataModel
                  {
                      Id = table.Column<int>(type: "INTEGER", nullable: false)
                         .Annotation("Fb:ValueGenerationStrategy", FbValueGenerationStrategy.IdentityColumn),
+                     PackName_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                     PackName_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                     PackType_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                     PackType_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                     StoragePlaceCode_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                     StoragePlaceCode_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                     StoragePlaceName_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                     StoragePlaceName_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
                      MassInPack_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
                      MassInPack_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
                      VolumeInPack_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
@@ -61,6 +69,14 @@ namespace Models.DBRealization.Migrations.DataModel
                 {
                     Id = table.Column<int>(type: "INTEGER", nullable: false)
                         .Annotation("Fb:ValueGenerationStrategy", FbValueGenerationStrategy.IdentityColumn),
+                    PackName_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    PackName_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    PackType_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    PackType_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceCode_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceCode_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceName_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceName_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
                     MassInPack_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
                     MassInPack_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
                     VolumeInPack_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
@@ -100,7 +116,7 @@ namespace Models.DBRealization.Migrations.DataModel
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            string b = "\"Id\",\"MassInPack_Hidden_Priv\",\"MassInPack_Hidden_Priv2\",\"VolumeInPack_Hidden_Priv\",\"VolumeInPack_Hidden_Priv2\",\"FcpNumber_Hidden_Priv\",\"Subsidy_Hidden_Priv\",\"Sum_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"PackName_DB\",\"PackType_DB\",\"CodeRAO_DB\",\"CodeRAO_Hidden_Priv\",\"StatusRAO_DB\",\"" +
+            string b = "\"Id\",\"PackName_Hidden_Priv\",\"PackName_Hidden_Priv2\",\"PackType_Hidden_Priv\",\"PackType_Hidden_Priv2\",\"StoragePlaceCode_Hidden_Priv\",\"StoragePlaceCode_Hidden_Priv2\",\"StoragePlaceName_Hidden_Priv\",\"StoragePlaceName_Hidden_Priv2\",\"MassInPack_Hidden_Priv\",\"MassInPack_Hidden_Priv2\",\"VolumeInPack_Hidden_Priv\",\"VolumeInPack_Hidden_Priv2\",\"FcpNumber_Hidden_Priv\",\"Subsidy_Hidden_Priv\",\"Sum_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"PackName_DB\",\"PackType_DB\",\"CodeRAO_DB\",\"CodeRAO_Hidden_Priv\",\"StatusRAO_DB\",\"" +
                "StatusRAO_Hidden_Priv\",\"VolumeInPack_DB\",\"MassInPack_DB\",\"VolumeOutOfPack_DB\",\"MassOutOfPack_DB\",\"QuantityOZIII_DB\",\"TritiumActivity_DB\",\"BetaGammaActivity_DB\"," +
                "\"AlphaActivity_DB\",\"TransuraniumActivity_DB\",\"MainRadionuclids_DB\",\"MainRadionuclids_Hidden_Priv\",\"Subsidy_DB\",\"FcpNumber_DB\",\"ReportId\",\"FormNum_DB\"," +
                "\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"CorrectionNumber_DB\"";
--- Models/DBRealization/Migrations/DataModel/20211221111134_DataModel_14.cs
+++ Models/DBRealization/Migrations/DataModel/20211221111134_DataModel_14.cs
@@ -7,7 +7,7 @@ namespace Models.DBRealization.Migrations.DataModel
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            string a = "\"Id\",\"MassInPack_Hidden_Priv\",\"MassInPack_Hidden_Priv2\",\"VolumeInPack_Hidden_Priv\",\"VolumeInPack_Hidden_Priv2\",\"FcpNumber_Hidden_Priv\",\"Subsidy_Hidden_Priv\",\"Sum_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"PackName_DB\",\"PackType_DB\",\"CodeRAO_DB\",\"CodeRAO_Hidden_Priv\",\"StatusRAO_DB\",\"" +
+            string a = "\"Id\",\"PackName_Hidden_Priv\",\"PackName_Hidden_Priv2\",\"PackType_Hidden_Priv\",\"PackType_Hidden_Priv2\",\"StoragePlaceCode_Hidden_Priv\",\"StoragePlaceCode_Hidden_Priv2\",\"StoragePlaceName_Hidden_Priv\",\"StoragePlaceName_Hidden_Priv2\",\"MassInPack_Hidden_Priv\",\"MassInPack_Hidden_Priv2\",\"VolumeInPack_Hidden_Priv\",\"VolumeInPack_Hidden_Priv2\",\"FcpNumber_Hidden_Priv\",\"Subsidy_Hidden_Priv\",\"Sum_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"PackName_DB\",\"PackType_DB\",\"CodeRAO_DB\",\"CodeRAO_Hidden_Priv\",\"StatusRAO_DB\",\"" +
                "StatusRAO_Hidden_Priv\",\"VolumeInPack_DB\",\"MassInPack_DB\",\"VolumeOutOfPack_DB\",\"MassOutOfPack_DB\",\"QuantityOZIII_DB\",\"TritiumActivity_DB\",\"BetaGammaActivity_DB\","+
                "\"AlphaActivity_DB\",\"TransuraniumActivity_DB\",\"MainRadionuclids_DB\",\"MainRadionuclids_Hidden_Priv\",\"Subsidy_DB\",\"FcpNumber_DB\",\"ReportId\",\"FormNum_DB\","+
                "\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"CorrectionNumber_DB\"";
@@ -20,6 +20,14 @@ namespace Models.DBRealization.Migrations.DataModel
                 {
                     Id = table.Column<int>(type: "INTEGER", nullable: false)
                         .Annotation("Fb:ValueGenerationStrategy", FbValueGenerationStrategy.IdentityColumn),
+                    PackName_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    PackName_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    PackType_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    PackType_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceCode_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceCode_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceName_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceName_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
                     MassInPack_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
                     MassInPack_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
                     VolumeInPack_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
@@ -65,7 +73,7 @@ namespace Models.DBRealization.Migrations.DataModel
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Restrict);
                 });
-            string b = "\"Id\",\"MassInPack_Hidden_Priv\",\"MassInPack_Hidden_Priv2\",\"VolumeInPack_Hidden_Priv\",\"VolumeInPack_Hidden_Priv2\",\"FcpNumber_Hidden_Priv\",\"Subsidy_Hidden_Priv\",\"Sum_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"PackName_DB\",\"PackType_DB\",\"PackQuantity_DB\",\"CodeRAO_DB\",\"CodeRAO_Hidden_Priv\",\"StatusRAO_DB\",\"" +
+            string b = "\"Id\",\"PackName_Hidden_Priv\",\"PackName_Hidden_Priv2\",\"PackType_Hidden_Priv\",\"PackType_Hidden_Priv2\",\"StoragePlaceCode_Hidden_Priv\",\"StoragePlaceCode_Hidden_Priv2\",\"StoragePlaceName_Hidden_Priv\",\"StoragePlaceName_Hidden_Priv2\",\"MassInPack_Hidden_Priv\",\"MassInPack_Hidden_Priv2\",\"VolumeInPack_Hidden_Priv\",\"VolumeInPack_Hidden_Priv2\",\"FcpNumber_Hidden_Priv\",\"Subsidy_Hidden_Priv\",\"Sum_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"PackName_DB\",\"PackType_DB\",\"PackQuantity_DB\",\"CodeRAO_DB\",\"CodeRAO_Hidden_Priv\",\"StatusRAO_DB\",\"" +
                "StatusRAO_Hidden_Priv\",\"VolumeInPack_DB\",\"MassInPack_DB\",\"VolumeOutOfPack_DB\",\"MassOutOfPack_DB\",\"QuantityOZIII_DB\",\"TritiumActivity_DB\",\"BetaGammaActivity_DB\"," +
                "\"AlphaActivity_DB\",\"TransuraniumActivity_DB\",\"MainRadionuclids_DB\",\"MainRadionuclids_Hidden_Priv\",\"Subsidy_DB\",\"FcpNumber_DB\",\"ReportId\",\"FormNum_DB\"," +
                "\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"CorrectionNumber_DB\"";
@@ -76,7 +84,7 @@ namespace Models.DBRealization.Migrations.DataModel
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            string a = "\"Id\",\"MassInPack_Hidden_Priv\",\"MassInPack_Hidden_Priv2\",\"VolumeInPack_Hidden_Priv\",\"VolumeInPack_Hidden_Priv2\",\"FcpNumber_Hidden_Priv\",\"Subsidy_Hidden_Priv\",\"Sum_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"PackName_DB\",\"PackType_DB\",\"CodeRAO_DB\",\"CodeRAO_Hidden_Priv\",\"StatusRAO_DB\",\"" +
+            string a = "\"Id\",\"PackName_Hidden_Priv\",\"PackName_Hidden_Priv2\",\"PackType_Hidden_Priv\",\"PackType_Hidden_Priv2\",\"StoragePlaceCode_Hidden_Priv\",\"StoragePlaceCode_Hidden_Priv2\",\"StoragePlaceName_Hidden_Priv\",\"StoragePlaceName_Hidden_Priv2\",\"MassInPack_Hidden_Priv\",\"MassInPack_Hidden_Priv2\",\"VolumeInPack_Hidden_Priv\",\"VolumeInPack_Hidden_Priv2\",\"FcpNumber_Hidden_Priv\",\"Subsidy_Hidden_Priv\",\"Sum_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"PackName_DB\",\"PackType_DB\",\"CodeRAO_DB\",\"CodeRAO_Hidden_Priv\",\"StatusRAO_DB\",\"" +
                "StatusRAO_Hidden_Priv\",\"VolumeInPack_DB\",\"MassInPack_DB\",\"VolumeOutOfPack_DB\",\"MassOutOfPack_DB\",\"QuantityOZIII_DB\",\"TritiumActivity_DB\",\"BetaGammaActivity_DB\"," +
                "\"AlphaActivity_DB\",\"TransuraniumActivity_DB\",\"MainRadionuclids_DB\",\"MainRadionuclids_Hidden_Priv\",\"Subsidy_DB\",\"FcpNumber_DB\",\"ReportId\",\"FormNum_DB\"," +
                "\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"CorrectionNumber_DB\"";
@@ -89,6 +97,14 @@ namespace Models.DBRealization.Migrations.DataModel
                 {
                     Id = table.Column<int>(type: "INTEGER", nullable: false)
                         .Annotation("Fb:ValueGenerationStrategy", FbValueGenerationStrategy.IdentityColumn),
+                    PackName_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    PackName_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    PackType_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    PackType_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceCode_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceCode_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceName_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
+                    StoragePlaceName_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
                     MassInPack_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
                     MassInPack_Hidden_Priv2 = table.Column<bool>(type: "BOOLEAN", nullable: false),
                     VolumeInPack_Hidden_Priv = table.Column<bool>(type: "BOOLEAN", nullable: false),
@@ -134,7 +150,7 @@ namespace Models.DBRealization.Migrations.DataModel
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Restrict);
                 });
-            string b = "\"Id\",\"MassInPack_Hidden_Priv\",\"MassInPack_Hidden_Priv2\",\"VolumeInPack_Hidden_Priv\",\"VolumeInPack_Hidden_Priv2\",\"FcpNumber_Hidden_Priv\",\"Subsidy_Hidden_Priv\",\"Sum_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"PackName_DB\",\"PackType_DB\",\"PackQuantity_DB\",\"CodeRAO_DB\",\"CodeRAO_Hidden_Priv\",\"StatusRAO_DB\",\"" +
+            string b = "\"Id\",\"PackName_Hidden_Priv\",\"PackName_Hidden_Priv2\",\"PackType_Hidden_Priv\",\"PackType_Hidden_Priv2\",\"StoragePlaceCode_Hidden_Priv\",\"StoragePlaceCode_Hidden_Priv2\",\"StoragePlaceName_Hidden_Priv\",\"StoragePlaceName_Hidden_Priv2\",\"MassInPack_Hidden_Priv\",\"MassInPack_Hidden_Priv2\",\"VolumeInPack_Hidden_Priv\",\"VolumeInPack_Hidden_Priv2\",\"FcpNumber_Hidden_Priv\",\"Subsidy_Hidden_Priv\",\"Sum_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"PackName_DB\",\"PackType_DB\",\"PackQuantity_DB\",\"CodeRAO_DB\",\"CodeRAO_Hidden_Priv\",\"StatusRAO_DB\",\"" +
                "StatusRAO_Hidden_Priv\",\"VolumeInPack_DB\",\"MassInPack_DB\",\"VolumeOutOfPack_DB\",\"MassOutOfPack_DB\",\"QuantityOZIII_DB\",\"TritiumActivity_DB\",\"BetaGammaActivity_DB\"," +
                "\"AlphaActivity_DB\",\"TransuraniumActivity_DB\",\"MainRadionuclids_DB\",\"MainRadionuclids_Hidden_Priv\",\"Subsidy_DB\",\"FcpNumber_DB\",\"ReportId\",\"FormNum_DB\"," +
                "\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"CorrectionNumber_DB\"";
