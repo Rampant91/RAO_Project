bc02e5955aefba7cfc1b88726ab4c4efaeacb0c1
kastetbro <32741355+kastetbro@users.noreply.github.com>
2022-01-14 13:41:51 +0300

    kdkdk

--- Models/DBRealization/Migrations/DataModel/20220110103756_DataModel_18.cs
+++ Models/DBRealization/Migrations/DataModel/20220110103756_DataModel_18.cs
@@ -8,13 +8,13 @@ namespace Models.DBRealization.Migrations.DataModel
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             #region form19
-            string b = "\"Id\",\"CodeTypeAccObject_DB\",\"Radionuclids_DB\",\"Activity_DB\",\"ReportId\",\"FormNum_DB\",\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"OperationCode_DB\",\"OperationCode_Hidden_Priv\",\"" +
+            string b = "\"CodeTypeAccObject_DB\",\"Radionuclids_DB\",\"Activity_DB\",\"ReportId\",\"FormNum_DB\",\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"OperationCode_DB\",\"OperationCode_Hidden_Priv\",\"" +
            "OperationDate_DB\",\"OperationDate_Hidden_Priv\",\"DocumentVid_DB\",\"DocumentVid_Hidden_Priv\",\"DocumentNumber_DB\",\"DocumentNumber_Hidden_Priv\",\"DocumentDate_DB\",\"DocumentDate_Hidden_Priv\"";
             migrationBuilder.Sql("INSERT INTO \"form_19\" (" + b + ") SELECT " + b + " FROM FORM_19_TEMP");
             migrationBuilder.DropTable("FORM_19_TEMP");
             #endregion
             #region form18
-            b = "\"Id\",\"Sum_DB\",\"IndividualNumberZHRO_DB\",\"IndividualNumberZHRO_Hidden_Pr~\",\"PassportNumber_DB\",\"PassportNumber_Hidden_Priv\",\"Volume6_DB\",\"Volume6_Hidden_Priv\",\"Mass7_DB\",\"" +
+            b = "\"Sum_DB\",\"IndividualNumberZHRO_DB\",\"IndividualNumberZHRO_Hidden_Pr~\",\"PassportNumber_DB\",\"PassportNumber_Hidden_Priv\",\"Volume6_DB\",\"Volume6_Hidden_Priv\",\"Mass7_DB\",\"" +
             "Mass7_Hidden_Priv\",\"SaltConcentration_DB\",\"SaltConcentration_Hidden_Priv\",\"Radionuclids_DB\",\"SpecificActivity_DB\",\"ProviderOrRecieverOKPO_DB\",\"ProviderOrRecieverOKPO_Hidden_~\",\"TransporterOKPO_DB\",\"TransporterOKPO_Hidden_Priv\",\"" +
             "StoragePlaceName_DB\",\"StoragePlaceName_Hidden_Priv\",\"StoragePlaceCode_DB\",\"StoragePlaceCode_Hidden_Priv\",\"CodeRAO_DB\",\"StatusRAO_DB\"," +
             "\"Volume20_DB\",\"Mass21_DB\",\"TritiumActivity_DB\",\"BetaGammaActivity_DB\",\"AlphaActivity_DB\",\"TransuraniumActivity_DB\",\"RefineOrSortRAOCode_DB\",\"Subsidy_DB\",\"FcpNumber_DB\",\"ReportId\",\"FormNum_DB\",\"" +
@@ -24,7 +24,7 @@ namespace Models.DBRealization.Migrations.DataModel
             migrationBuilder.DropTable("FORM_18_TEMP");
             #endregion
             #region form17
-            b = "\"Id\",\"Sum_DB\",\"PackName_DB\",\"PackName_Hidden_Priv\",\"PackType_DB\",\"PackType_Hidden_Priv\",\"PackNumber_DB\",\"PackNumber_Hidden_Priv\",\"PackFactoryNumber_DB\",\"" +
+            b = "\"Sum_DB\",\"PackName_DB\",\"PackName_Hidden_Priv\",\"PackType_DB\",\"PackType_Hidden_Priv\",\"PackNumber_DB\",\"PackNumber_Hidden_Priv\",\"PackFactoryNumber_DB\",\"" +
             "PackFactoryNumber_Hidden_Priv\",\"FormingDate_DB\",\"FormingDate_Hidden_Priv\",\"Volume_DB\",\"Volume_Hidden_Priv\",\"Mass_DB\",\"Mass_Hidden_Priv\",\"PassportNumber_DB\",\"PassportNumber_Hidden_Priv\",\"" +
             "Radionuclids_DB\",\"SpecificActivity_DB\",\"ProviderOrRecieverOKPO_DB\",\"ProviderOrRecieverOKPO_Hidden_~\",\"TransporterOKPO_DB\",\"TransporterOKPO_Hidden_Priv\",\"StoragePlaceName_DB\",\"" +
             "StoragePlaceName_Hidden_Priv\",\"StoragePlaceCode_DB\",\"StoragePlaceCode_Hidden_Priv\",\"Subsidy_DB\",\"FcpNumber_DB\",\"CodeRAO_DB\",\"StatusRAO_DB\",\"VolumeOutOfPack_DB\",\"MassOutOfPack_DB\",\"Quantity_DB\",\"TritiumActivity_DB\",\"BetaGammaActivity_DB\",\"AlphaActivity_DB\",\"TransuraniumActivity_DB\",\"RefineOrSortRAOCode_DB\",\"ReportId\",\"FormNum_DB\",\"NumberInOrder_DB\",\"" +
@@ -34,7 +34,7 @@ namespace Models.DBRealization.Migrations.DataModel
             migrationBuilder.DropTable("FORM_17_TEMP");
             #endregion
             #region form16
-            b = "\"Id\",\"CodeRAO_DB\",\"StatusRAO_DB\",\"Volume_DB\",\"Mass_DB\",\"MainRadionuclids_DB\",\"TritiumActivity_DB\",\"BetaGammaActivity_DB\",\"AlphaActivity_DB\",\"TransuraniumActivity_DB\",\"ActivityMeasurementDate_DB\",\"QuantityOZIII_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"" +
+            b = "\"CodeRAO_DB\",\"StatusRAO_DB\",\"Volume_DB\",\"Mass_DB\",\"MainRadionuclids_DB\",\"TritiumActivity_DB\",\"BetaGammaActivity_DB\",\"AlphaActivity_DB\",\"TransuraniumActivity_DB\",\"ActivityMeasurementDate_DB\",\"QuantityOZIII_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"" +
             "StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"Subsidy_DB\",\"FcpNumber_DB\",\"RefineOrSortRAOCode_DB\",\"ReportId\",\"FormNum_DB\",\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"" +
             "OperationCode_DB\",\"OperationCode_Hidden_Priv\",\"OperationDate_DB\",\"OperationDate_Hidden_Priv\",\"DocumentVid_DB\",\"DocumentVid_Hidden_Priv\",\"" +
             "DocumentNumber_DB\",\"DocumentNumber_Hidden_Priv\",\"DocumentDate_DB\",\"DocumentDate_Hidden_Priv\"";
@@ -42,7 +42,7 @@ namespace Models.DBRealization.Migrations.DataModel
             migrationBuilder.DropTable("FORM_16_TEMP");
             #endregion
             #region form15
-            b = "\"Id\",\"PassportNumber_DB\",\"Type_DB\",\"Radionuclids_DB\",\"FactoryNumber_DB\",\"Quantity_DB\",\"Activity_DB\",\"CreationDate_DB\",\"StatusRAO_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"RefineOrSortRAOCode_DB\",\"" +
+            b = "\"PassportNumber_DB\",\"Type_DB\",\"Radionuclids_DB\",\"FactoryNumber_DB\",\"Quantity_DB\",\"Activity_DB\",\"CreationDate_DB\",\"StatusRAO_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"StoragePlaceName_DB\",\"StoragePlaceCode_DB\",\"RefineOrSortRAOCode_DB\",\"" +
             "Subsidy_DB\",\"FcpNumber_DB\",\"ReportId\",\"FormNum_DB\",\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"OperationCode_DB\",\"OperationCode_Hidden_Priv\",\"OperationDate_DB\",\"OperationDate_Hidden_Priv\",\"" +
             "DocumentVid_DB\",\"DocumentVid_Hidden_Priv\",\"DocumentNumber_DB\",\"DocumentNumber_Hidden_Priv\",\"DocumentDate_DB\",\"" +
             "DocumentDate_Hidden_Priv\"";
@@ -50,13 +50,13 @@ namespace Models.DBRealization.Migrations.DataModel
             migrationBuilder.DropTable("FORM_15_TEMP");
             #endregion
             #region form14
-            b = "\"Id\",\"PassportNumber_DB\",\"Name_DB\",\"Sort_DB\",\"Radionuclids_DB\",\"Activity_DB\",\"ActivityMeasurementDate_DB\",\"Volume_DB\",\"Mass_DB\",\"AggregateState_DB\",\"PropertyCode_DB\",\"Owner_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"" +
+            b = "\"PassportNumber_DB\",\"Name_DB\",\"Sort_DB\",\"Radionuclids_DB\",\"Activity_DB\",\"ActivityMeasurementDate_DB\",\"Volume_DB\",\"Mass_DB\",\"AggregateState_DB\",\"PropertyCode_DB\",\"Owner_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"" +
             "ReportId\",\"FormNum_DB\",\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"OperationCode_DB\",\"OperationCode_Hidden_Priv\",\"OperationDate_DB\",\"OperationDate_Hidden_Priv\",\"DocumentVid_DB\",\"DocumentVid_Hidden_Priv\",\"DocumentNumber_DB\",\"DocumentNumber_Hidden_Priv\",\"DocumentDate_DB\",\"DocumentDate_Hidden_Priv\"";
             migrationBuilder.Sql("INSERT INTO \"form_14\" (" + b + ") SELECT " + b + " FROM FORM_14_TEMP");
             migrationBuilder.DropTable("FORM_14_TEMP");
             #endregion
             #region form13
-            b = "\"Id\",\"PassportNumber_DB\",\"Type_DB\",\"Radionuclids_DB\",\"FactoryNumber_DB\",\"Activity_DB\",\"CreationDate_DB\",\"CreatorOKPO_DB\",\"AggregateState_DB\",\"PropertyCode_DB\",\"Owner_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"" +
+            b = "\"PassportNumber_DB\",\"Type_DB\",\"Radionuclids_DB\",\"FactoryNumber_DB\",\"Activity_DB\",\"CreationDate_DB\",\"CreatorOKPO_DB\",\"AggregateState_DB\",\"PropertyCode_DB\",\"Owner_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"" +
             "ReportId\",\"FormNum_DB\",\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"OperationCode_DB\",\"OperationCode_Hidden_Priv\",\"OperationDate_DB\",\"OperationDate_Hidden_Priv\",\"" +
             "DocumentVid_DB\",\"DocumentVid_Hidden_Priv\",\"DocumentNumber_DB\",\"DocumentNumber_Hidden_Priv\",\"DocumentDate_DB\",\"" +
             "DocumentDate_Hidden_Priv\"";
@@ -64,7 +64,7 @@ namespace Models.DBRealization.Migrations.DataModel
             migrationBuilder.DropTable("FORM_13_TEMP");
             #endregion
             #region form12
-            b = "\"Id\",\"PassportNumber_DB\",\"NameIOU_DB\",\"FactoryNumber_DB\",\"Mass_DB\",\"CreatorOKPO_DB\",\"CreationDate_DB\",\"SignedServicePeriod_DB\",\"PropertyCode_DB\",\"Owner_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"" +
+            b = "\"PassportNumber_DB\",\"NameIOU_DB\",\"FactoryNumber_DB\",\"Mass_DB\",\"CreatorOKPO_DB\",\"CreationDate_DB\",\"SignedServicePeriod_DB\",\"PropertyCode_DB\",\"Owner_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"" +
             "ReportId\",\"FormNum_DB\",\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"OperationCode_DB\",\"OperationCode_Hidden_Priv\",\"OperationDate_DB\",\"OperationDate_Hidden_Priv\",\"" +
             "DocumentVid_DB\",\"DocumentVid_Hidden_Priv\",\"DocumentNumber_DB\",\"DocumentNumber_Hidden_Priv\",\"DocumentDate_DB\",\"" +
             "DocumentDate_Hidden_Priv\"";
@@ -72,7 +72,7 @@ namespace Models.DBRealization.Migrations.DataModel
             migrationBuilder.DropTable("FORM_12_TEMP");
             #endregion
             #region form11
-            b = "\"Id\",\"PassportNumber_DB\",\"Type_DB\",\"Radionuclids_DB\",\"FactoryNumber_DB\",\"Quantity_DB\",\"Activity_DB\",\"CreationDate_DB\",\"CreatorOKPO_DB\",\"Category_DB\",\"SignedServicePeriod_DB\",\"PropertyCode_DB\",\"Owner_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"" +
+            b = "\"PassportNumber_DB\",\"Type_DB\",\"Radionuclids_DB\",\"FactoryNumber_DB\",\"Quantity_DB\",\"Activity_DB\",\"CreationDate_DB\",\"CreatorOKPO_DB\",\"Category_DB\",\"SignedServicePeriod_DB\",\"PropertyCode_DB\",\"Owner_DB\",\"ProviderOrRecieverOKPO_DB\",\"TransporterOKPO_DB\",\"PackName_DB\",\"PackType_DB\",\"PackNumber_DB\",\"" +
             "ReportId\",\"FormNum_DB\",\"NumberInOrder_DB\",\"NumberOfFields_DB\",\"OperationCode_DB\",\"OperationCode_Hidden_Priv\",\"OperationDate_DB\",\"OperationDate_Hidden_Priv\",\"" +
             "DocumentVid_DB\",\"DocumentVid_Hidden_Priv\",\"DocumentNumber_DB\",\"DocumentNumber_Hidden_Priv\",\"DocumentDate_DB\",\"" +
             "DocumentDate_Hidden_Priv\"";
