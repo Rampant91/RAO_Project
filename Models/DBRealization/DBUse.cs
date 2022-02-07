d7d433ffc3b0cf0fa0d60ff09a2673b4f2b564b9
Alex Götzef <g0tzef@yandex.ru>
2022-02-07 13:37:56 +0300

    fix

--- Models/DBRealization/DBUse.cs
+++ Models/DBRealization/DBUse.cs
@@ -11,12 +11,12 @@ namespace Models.DBRealization
     public class DBUse
     {
         #region GetData10Main
-        public static async Task<ObservableCollectionWithItemPropertyChanged<IKey>> GetData10MainAsync()
+        public static async Task<ObservableCollectionWithItemPropertyChanged<Reports>> GetData10MainAsync()
         {
             using (var db = new DBModel(StaticConfiguration.DBPath))
             {
                 await db.Database.MigrateAsync();
-                return new ObservableCollectionWithItemPropertyChanged<IKey>(await db.ReportsCollectionDbSet
+                return new ObservableCollectionWithItemPropertyChanged<Reports>(await db.ReportsCollectionDbSet
                     .Where(x => x.Master_DB.FormNum_DB == "1.0")
                     .Include(x => x.Master_DB).ThenInclude(x => x.Rows10)
                     .Include(x => x.Report_Collection)
@@ -61,6 +61,116 @@ namespace Models.DBRealization
                     .Include(x => x.Master_DB).ThenInclude(x => x.Rows20)
                     .Include(x => x.Report_Collection)
                     .ToList());
+            }
+
+        }
+        #endregion
+
+        #region GetDataForm
+        public static async Task<Report> GetDataFormAsync(int ID)
+        {
+            using (var db = new DBModel(StaticConfiguration.DBPath))
+            {
+                await db.Database.MigrateAsync();
+                return await db.ReportCollectionDbSet
+                    .Where(x => x.Id == ID)
+                    .FirstOrDefaultAsync();
+            }
+        }
+        public static Report GetDataForm(int ID)
+        {
+            using (var db = new DBModel(StaticConfiguration.DBPath))
+            {
+                db.Database.Migrate();
+                return db.ReportCollectionDbSet
+                    .Where(x => x.Id == ID)
+                    .FirstOrDefault();
+            }
+        }
+        #endregion
+
+        #region GetData10Org
+        public static async Task<Reports> GetData10OrgAsync(int ID)
+        {
+            using (var db = new DBModel(StaticConfiguration.DBPath))
+            {
+                await db.Database.MigrateAsync();
+                return await db.ReportsCollectionDbSet
+                    .Where(x => x.Id == ID && x.Master_DB.FormNum_DB == "1.0")
+                    .Include(x => x.Master_DB).ThenInclude(x => x.Rows10)
+                    .FirstOrDefaultAsync();
+            }
+        }
+        public static Reports GetData10Org(int ID)
+        {
+            using (var db = new DBModel(StaticConfiguration.DBPath))
+            {
+                db.Database.Migrate();
+                return db.ReportsCollectionDbSet
+                    .Where(x => x.Id == ID && x.Master_DB.FormNum_DB == "1.0")
+                    .Include(x => x.Master_DB).ThenInclude(x => x.Rows10)
+                    .FirstOrDefault();
+            }
+        }
+        #endregion
+
+        #region GetData20Org
+        public static async Task<Reports> GetData20OrgAsync(int ID)
+        {
+            using (var db = new DBModel(StaticConfiguration.DBPath))
+            {
+                await db.Database.MigrateAsync();
+                return await db.ReportsCollectionDbSet
+                    .Where(x => x.Id == ID && x.Master_DB.FormNum_DB == "2.0")
+                    .Include(x => x.Master_DB).ThenInclude(x => x.Rows10)
+                    .FirstOrDefaultAsync();
+            }
+        }
+        public static Reports GetData20Org(int ID)
+        {
+            using (var db = new DBModel(StaticConfiguration.DBPath))
+            {
+                db.Database.Migrate();
+                return db.ReportsCollectionDbSet
+                    .Where(x => x.Id == ID && x.Master_DB.FormNum_DB == "2.0")
+                    .Include(x => x.Master_DB).ThenInclude(x => x.Rows10)
+                    .FirstOrDefault();
+            }
+        }
+        #endregion
+
+
+        #region SetData10Main
+        public static void SetData10Main(ObservableCollectionWithItemPropertyChanged<Reports> new_reps)
+        {
+            using (var db = new DBModel(StaticConfiguration.DBPath))
+            {
+                db.Database.Migrate();
+                var cur_reps = new ObservableCollectionWithItemPropertyChanged<Reports>(db.ReportsCollectionDbSet
+                    .Where(x => x.Master_DB.FormNum_DB == "1.0")
+                    .Include(x => x.Master_DB).ThenInclude(x => x.Rows10)
+                    .Include(x => x.Report_Collection)
+                    .ToList());
+
+                foreach (Reports reps in new_reps)
+                {
+                    var notIn = true;
+                    foreach (Reports reps1 in cur_reps)
+                    {
+                        if (reps.Id == reps1.Id)
+                        {
+                            cur_reps.Remove(reps1);
+                            cur_reps.Add(reps);
+                            notIn = false;
+                        }
+                    }
+                    if (notIn)
+                    {
+                        cur_reps.Add(reps);
+                    }
+                }
+
+                db.SaveChanges();
             }
 
         }
