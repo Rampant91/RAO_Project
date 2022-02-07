047884a7946c3f90482efdb49fca88b7bdeaa220
Alex Götzef <g0tzef@yandex.ru>
2022-02-07 15:02:31 +0300

    test

---
+++ Models/DBRealization/DBAPIFactory/ReportEssence.cs
@@ -0,0 +1,20 @@
+﻿using Models.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.DBRealization.DBAPIFactory
+{
+    internal class ReportEssence : IEssence
+    {
+        public T Post<T>(T obj) where T : class, IKey
+        {
+            return null;
+        }
+        public void Get(int ID) { }
+        public void Update(int ID) { }
+        public void Delete(int ID) { }
+    }
+}
