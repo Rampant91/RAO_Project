fea7df29177c2cc1827f7d0f14a862683ee0b519
Alex Götzef <g0tzef@yandex.ru>
2022-03-09 10:04:06 +0300

    Revert "fix"
    This reverts commit 5f5d86630fa18f3cc66cdc9b0d3d1f982e7d5209.

--- Models/Forms/Form1/Form17.cs
+++ Models/Forms/Form1/Form17.cs
@@ -2057,9 +2057,7 @@ namespace Models
             bool a9 = value.Value == "99";
             bool a10 = (int.Parse(value.Value) >= 21) && (int.Parse(value.Value) <= 29);
             bool a11 = (int.Parse(value.Value) >= 31) && (int.Parse(value.Value) <= 39);
-            bool a12 = value.Value == "51";
-            bool a13 = value.Value == "52";
-            if (!(a0 || a1 || a2 || a3 || a4 || a5 || a6 || a7 || a8 || a9 || a10 || a11 || a12 || a13))
+            if (!(a0 || a1 || a2 || a3 || a4 || a5 || a6 || a7 || a8 || a9 || a10 || a11))
             {
                 value.AddError("Код операции не может быть использован в форме 1.7");
                 return false;
