45d885b7446776fc5f2373fff7a9546b6db0d6bf
Alex Götzef <g0tzef@yandex.ru>
2022-03-21 10:56:07 +0300

    fix

--- Models/Forms/Form2/Form212.cs
+++ Models/Forms/Form2/Form212.cs
@@ -441,6 +441,7 @@ namespace Models
             {
                 #region NumberInOrder (1)
                 DataGridColumns NumberInOrderR = ((Attributes.Form_PropertyAttribute)typeof(Form).GetProperty(nameof(Form.NumberInOrder)).GetCustomAttributes(typeof(Attributes.Form_PropertyAttribute), true).FirstOrDefault()).GetDataColumnStructureD();
+                NumberInOrderR.SetSizeColToAllLevels(88);
                 NumberInOrderR.Binding = nameof(Form.NumberInOrder);
                 NumberInOrderR.Blocked = true;
                 NumberInOrderR.ChooseLine = true;
