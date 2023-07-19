31fd1b6776972046ead0a8ae23478ea59d03c989
Rampant91 <rampant91@gmail.com>
2023-07-19 14:41:49 +0300

    ExcelExport Executors done

--- Client_App/ViewModels/BaseVM.cs
+++ Client_App/ViewModels/BaseVM.cs
@@ -16,7 +16,7 @@ public class BaseVM
 {
     internal static string PasFolderPath = @"Y:\!!! Поручения\Паспорта ЗРИ 2022\Хранилище паспортов ЗРИ";
 
-    internal const string Version = @"1.2.2.12";
+    internal const string Version = @"1.2.2.13";
 
     internal static string DbFileName = "Local_0";
 
