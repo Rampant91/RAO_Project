1ed90afc4dec6e106e98142ab8bf7d436300e5d3
Rampant91 <rampant91@gmail.com>
2023-10-17 11:06:23 +0300

    fix json21 & 22 model
    чёрт побери тех, кто нумеровал колонки

--- Models/JSON/TableDataMain/TableDataMain22.cs
+++ Models/JSON/TableDataMain/TableDataMain22.cs
@@ -11,49 +11,49 @@ public class TableDataMain22
     [JsonProperty("g3")] public string StoragePlaceCode { get; set; }
 
     //"УКТ, упаковка ли иная учетная единица", "наименование", "4"
-    [JsonProperty("g4")] public string PackName { get; set; }
+    [JsonProperty("g11")] public string PackName { get; set; }
 
     //"УКТ, упаковка ли иная учетная единица", "тип", "5"
-    [JsonProperty("g5")] public string PackType { get; set; }
+    [JsonProperty("g12")] public string PackType { get; set; }
 
     //"УКТ, упаковка ли иная учетная единица", "количество, шт", "6"
-    [JsonProperty("g6")] public string PackQuantity { get; set; }
+    [JsonProperty("g13")] public string PackQuantity { get; set; }
 
     //"null-7", "Код РАО", "7"
-    [JsonProperty("g7")] public string CodeRAO { get; set; }
+    [JsonProperty("g4")] public string CodeRAO { get; set; }
 
     //"null-8", "Статус РАО", "8"
-    [JsonProperty("g8")] public string StatusRAO { get; set; }
+    [JsonProperty("g5")] public string StatusRAO { get; set; }
 
     //"Объем, куб. м", "РАО без упаковки", "9"
-    [JsonProperty("g9")] public string VolumeOutOfPack { get; set; }
+    [JsonProperty("g6")] public string VolumeOutOfPack { get; set; }
 
     //"Объем, куб. м", "РАО с упаковкой", "10"
-    [JsonProperty("g10")] public string VolumeInPack { get; set; }
+    [JsonProperty("g7")] public string VolumeInPack { get; set; }
 
     //"Масса, т", "РАО без упаковки (нетто)", "11"
-    [JsonProperty("g11")] public string MassOutOfPack { get; set; }
+    [JsonProperty("g8")] public string MassOutOfPack { get; set; }
 
     //"Масса, т", "РАО с упаковкой (брутто)", "12"
-    [JsonProperty("g12")] public string MassInPack { get; set; }
+    [JsonProperty("g9")] public string MassInPack { get; set; }
 
     //"null-13", "Количество ОЗИИИ, шт", "13"
-    [JsonProperty("g13")] public string QuantityOZIII { get; set; }
+    [JsonProperty("g10")] public string QuantityOZIII { get; set; }
 
     //"Суммарная активность, Бк", "тритий", "14"
-    [JsonProperty("g14")] public string TritiumActivity { get; set; }
+    [JsonProperty("g19")] public string TritiumActivity { get; set; }
 
     //"Суммарная активность, Бк", "бета-, гамма-излучающие радионуклиды (исключая тритий)", "15"
     [JsonProperty("g15")] public string BetaGammaActivity { get; set; }
 
     //"Суммарная активность, Бк", "альфа-излучающие радионуклиды (исключая трансурановые)", "16"
-    [JsonProperty("g16")] public string AlphaActivity { get; set; }
+    [JsonProperty("g14")] public string AlphaActivity { get; set; }
 
     //"Суммарная активность, Бк", "трансурановые радионуклиды", "17"
-    [JsonProperty("g19")] public string TransuraniumActivity { get; set; }
+    [JsonProperty("g20")] public string TransuraniumActivity { get; set; }
 
     //"null-18", "Основные радионуклиды", "18"
-    [JsonProperty("g20")] public string MainRadionuclids { get; set; }
+    [JsonProperty("g16")] public string MainRadionuclids { get; set; }
 
     //"null-19", "Субсидия, %", "19"
     [JsonProperty("g21")] public string Subsidy { get; set; }
