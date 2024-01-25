ff7eb424249b8c38de48bd517be39700e68b897c
Rampant91 <rampant91@gmail.com>
2024-01-25 12:15:25 +0300

    it worl

--- Client_App/Commands/AsyncCommands/Delete/DeleteFormAsyncCommand.cs
+++ Client_App/Commands/AsyncCommands/Delete/DeleteFormAsyncCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using Client_App.Views;
 using Models.Collections;
 using Models.DBRealization;
@@ -51,51 +52,74 @@ internal class DeleteFormAsyncCommand : BaseAsyncCommand
             if (selectedReport is null) return;
             await using var db = new DBModel(StaticConfiguration.DBPath);
 
-            var forms11IdToDelete = db.form_11.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
-            foreach (var form11 in forms11IdToDelete.Select(formId => new Form11 { Id = formId }))
+            switch (selectedReport.FormNum_DB)
             {
-                db.form_11.Remove(form11);
+                case "1.1":
+                    var forms11IdToDelete = db.form_11.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
+                    foreach (var form11 in forms11IdToDelete.Select(formId => new Form11 { Id = formId }))
+                    {
+                        db.form_11.Remove(form11);
+                    }
+                    break;
+                case "1.2":
+                    var forms12IdToDelete = db.form_12.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
+                    foreach (var form12 in forms12IdToDelete.Select(formId => new Form12 { Id = formId }))
+                    {
+                        db.form_12.Remove(form12);
+                    }
+                    break;
+                case "1.3":
+                    var forms13IdToDelete = db.form_13.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
+                    foreach (var form13 in forms13IdToDelete.Select(formId => new Form13 { Id = formId }))
+                    {
+                        db.form_13.Remove(form13);
+                    }
+                    break;
+                case "1.4":
+                    var forms14IdToDelete = db.form_14.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
+                    foreach (var form14 in forms14IdToDelete.Select(formId => new Form14 { Id = formId }))
+                    {
+                        db.form_14.Remove(form14);
+                    }
+                    break;
+                case "1.5":
+                    var forms15IdToDelete = db.form_15.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
+                    foreach (var form15 in forms15IdToDelete.Select(formId => new Form15 { Id = formId }))
+                    {
+                        db.form_15.Remove(form15);
+                    }
+                    break;
+                case "1.6":
+                    var forms16IdToDelete = db.form_16.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
+                    foreach (var form16 in forms16IdToDelete.Select(formId => new Form16 { Id = formId }))
+                    {
+                        db.form_16.Remove(form16);
+                    }
+                    break;
+                case "1.7":
+                    var forms17IdToDelete = db.form_17.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
+                    foreach (var form17 in forms17IdToDelete.Select(formId => new Form17 { Id = formId }))
+                    {
+                        db.form_17.Remove(form17);
+                    }
+                    break;
+                case "1.8":
+                    var forms18IdToDelete = db.form_18.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
+                    foreach (var form18 in forms18IdToDelete.Select(formId => new Form18 { Id = formId }))
+                    {
+                        db.form_18.Remove(form18);
+                    }
+                    break;
+                case "1.9":
+                    var forms19IdToDelete = db.form_19.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
+                    foreach (var form19 in forms19IdToDelete.Select(formId => new Form19 { Id = formId }))
+                    {
+                        db.form_19.Remove(form19);
+                    }
+                    break;
+                default: return;
             }
-            var forms12IdToDelete = db.form_11.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
-            foreach (var form12 in forms12IdToDelete.Select(formId => new Form12 { Id = formId }))
-            {
-                db.form_12.Remove(form12);
-            }
-            var forms13IdToDelete = db.form_11.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
-            foreach (var form13 in forms13IdToDelete.Select(formId => new Form13 { Id = formId }))
-            {
-                db.form_13.Remove(form13);
-            }
-            var forms14IdToDelete = db.form_11.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
-            foreach (var form14 in forms14IdToDelete.Select(formId => new Form14 { Id = formId }))
-            {
-                db.form_14.Remove(form14);
-            }
-            var forms15IdToDelete = db.form_11.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
-            foreach (var form15 in forms15IdToDelete.Select(formId => new Form15 { Id = formId }))
-            {
-                db.form_15.Remove(form15);
-            }
-            var forms16IdToDelete = db.form_11.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
-            foreach (var form16 in forms16IdToDelete.Select(formId => new Form16 { Id = formId }))
-            {
-                db.form_16.Remove(form16);
-            }
-            var forms17IdToDelete = db.form_11.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
-            foreach (var form17 in forms17IdToDelete.Select(formId => new Form17 { Id = formId }))
-            {
-                db.form_17.Remove(form17);
-            }
-            var forms18IdToDelete = db.form_11.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
-            foreach (var form18 in forms18IdToDelete.Select(formId => new Form18 { Id = formId }))
-            {
-                db.form_18.Remove(form18);
-            }
-            var forms19IdToDelete = db.form_11.Where(x => x.ReportId == selectedReport.Id).Select(x => x.Id).ToList();
-            foreach (var form19 in forms19IdToDelete.Select(formId => new Form19 { Id = formId }))
-            {
-                db.form_19.Remove(form19);
-            }
+            await db.SaveChangesAsync();
 
             db.ReportCollectionDbSet.Remove(selectedReport);
             var selectedReports = mainWindow!.SelectedReports.First() as Reports;
