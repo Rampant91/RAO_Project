c2d9b1cb30bc262bd68b85ace01c63bb684018ec
Rampant91 <rampant91@gmail.com>
2024-04-17 15:40:04 +0300

    ....

--- Client_App/Commands/AsyncCommands/RaodbExport/ExportAllReportsOneFileAsyncCommand.cs
+++ Client_App/Commands/AsyncCommands/RaodbExport/ExportAllReportsOneFileAsyncCommand.cs
@@ -54,12 +54,16 @@ public partial class ExportAllReportsOneFileAsyncCommand : BaseAsyncCommand
 
         try
         {
+            var i = 0;
             foreach (var reps in ReportsStorage.LocalReports.Reports_Collection)
             {
                 var exportOrg = (Reports)reps;
 
-                GC.Collect();
-                GC.WaitForPendingFinalizers();
+                if (i % 100 == 0)
+                {
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                }
 
                 var oldReps = await db.ReportsCollectionDbSet.FindAsync(exportOrg.Id);
                 if (oldReps != null) db.ReportsCollectionDbSet.Remove(oldReps);
@@ -76,6 +80,7 @@ public partial class ExportAllReportsOneFileAsyncCommand : BaseAsyncCommand
 
                 await db.ReportsCollectionDbSet.AddAsync(exportOrg);
                 await db.SaveChangesAsync();
+                i++;
             }
             var t = db.Database.GetDbConnection() as FbConnection;
             await t.CloseAsync();
