3066f167ad841f10b920557ef5b6f0b1209ba557
Rampant91 <rampant91@gmail.com>
2024-04-18 11:44:52 +0300

    Migration works

---
+++ Models/DBRealization/Migrations/DataModel/20240418084009_DataModel_28.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Models.DBRealization.Migrations.DataModel
+{
+    public partial class DataModel_28 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "StartPeriodNew_DB",
+                table: "ReportCollection_DbSet",
+                type: "varchar(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartPeriodNew_DB",
+                table: "ReportCollection_DbSet");
+        }
+    }
+}
---
+++ Models/DBRealization/Migrations/DataModel/20240418084026_DataModel_29.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Models.DBRealization.Migrations.DataModel
+{
+    public partial class DataModel_29 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"ReportCollection_DbSet\" SET \"StartPeriodNew_DB\" = \"StartPeriod_DB\"");
+
+            migrationBuilder.DropColumn(
+                name: "StartPeriod_DB",
+                table: "ReportCollection_DbSet");
+
+            migrationBuilder.RenameColumn(
+                name: "StartPeriodNew_DB",
+                table: "ReportCollection_DbSet",
+                newName: "StartPeriod_DB");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
