764634d9a2bb3fc38b893c0d3fdcced2f8bafbe1
Rampant91 <97747455+Rampant91@users.noreply.github.com>
2024-09-30 16:37:26 +0300

    config works

--- Client_App/Commands/AsyncCommands/Passports/ChangePasFolderAsyncCommand.cs
+++ Client_App/Commands/AsyncCommands/Passports/ChangePasFolderAsyncCommand.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia.Controls;
 using System.Threading.Tasks;
+using Client_App.Properties;
 using Client_App.ViewModels;
 
 namespace Client_App.Commands.AsyncCommands.Passports;
@@ -9,7 +10,9 @@ public class ChangePasFolderAsyncCommand : BaseAsyncCommand
 {
     public override async Task AsyncExecute(object? parameter)
     {
-        OpenFolderDialog openFolderDialog = new() { Directory = BaseVM.PasFolderPath };
+        OpenFolderDialog openFolderDialog = new() { Directory = Settings.Default.PasFolderDefaultPath };
         BaseVM.PasFolderPath = await openFolderDialog.ShowAsync(Desktop.MainWindow) ?? BaseVM.PasFolderPath;
+        Settings.Default.PasFolderDefaultPath = BaseVM.PasFolderPath;
+        Settings.Default.Save();
     }
 }
