ce2457aed1ab09cafdba2d30f5975e1d99848252
shaihurov <me@shaihnurov.ru>
2024-12-26 09:09:10 +0300

    Добавление RadionuclidDTO

---
+++ Models/DTO/RadionuclidDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Models.DTO
+{
+    public class RadionuclidDTO
+    {
+        public string Name { get; set; }
+        public string CodeNumber { get; set; }
+        public string? HalfLife { get; set; }
+        public string? Unit { get; set; }
+    }
+}
