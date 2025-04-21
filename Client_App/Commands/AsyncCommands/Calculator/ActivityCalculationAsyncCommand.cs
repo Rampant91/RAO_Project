52c29c0490449de952bbd6a5aeb45c56a4138f20
Rampant91 <97747455+Rampant91@users.noreply.github.com>
2025-04-21 16:27:19 +0300

    Calculator done

---
+++ Client_App/Commands/AsyncCommands/Calculator/ActivityCalculationAsyncCommand.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Client_App.ViewModels.Calculator;
+using System.Linq;
+using System.Threading.Tasks;
+using ExtendedNumerics;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Client_App.Commands.AsyncCommands.Calculator;
+
+public partial class ActivityCalculationAsyncCommand : BaseAsyncCommand
+{
+    private readonly ActivityCalculatorVM _activityCalculatorVM;
+
+    public ActivityCalculationAsyncCommand(ActivityCalculatorVM activityCalculatorVM)
+    {
+        _activityCalculatorVM = activityCalculatorVM;
+        _activityCalculatorVM.PropertyChanged += ActivityCalculatorVMPropertyChanged;
+    }
+
+    private void ActivityCalculatorVMPropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        if (e.PropertyName is nameof(ActivityCalculatorVM.SelectedNuclid.Halflife) 
+            or nameof(ActivityCalculatorVM.SelectedNuclid.Unit) 
+            or nameof(ActivityCalculatorVM.InitialActivity) 
+            or nameof(ActivityCalculatorVM.TimePeriodDouble) 
+            or nameof(ActivityCalculatorVM.SelectedTimeUnit))
+        {
+            OnCanExecuteChanged();
+        }
+    }
+
+    public override Task AsyncExecute(object? parameter)
+    {
+        if (_activityCalculatorVM is { IsDateRange: false })
+        {
+            if (double.TryParse(_activityCalculatorVM.TimePeriodDouble, out var timePeriodDoubleValue)
+                && double.TryParse(ToExponentialString(_activityCalculatorVM.InitialActivity), out var initialActivityDoubleValue))
+            {
+                var halfLife = GetTimeDoubleValueInMinutes(_activityCalculatorVM.SelectedNuclid.Halflife, _activityCalculatorVM.SelectedNuclid.Unit);
+                var timePeriod = GetTimeDoubleValueInMinutes(timePeriodDoubleValue, _activityCalculatorVM.SelectedTimeUnit);
+                var cycles = timePeriod / halfLife;
+                if (cycles > 10000) cycles = 10000;
+                var activity = initialActivityDoubleValue / BigDecimal.Pow(2, cycles);
+                _activityCalculatorVM.ResidualActivity = ToExponentialString(activity);
+            }
+            else _activityCalculatorVM.ResidualActivity = string.Empty;
+        }
+        else if (_activityCalculatorVM is { IsDateRange: true })
+        {
+            if (double.TryParse(ToExponentialString(_activityCalculatorVM.InitialActivity), out var initialActivityDoubleValue) 
+                && DateOnly.TryParse(_activityCalculatorVM.InitialActivityDate, out var initialActivityDate) 
+                && DateOnly.TryParse(_activityCalculatorVM.ResidualActivityDate, out var residualActivityDate))
+            {
+                var halfLife = GetTimeDoubleValueInMinutes(_activityCalculatorVM.SelectedNuclid.Halflife, _activityCalculatorVM.SelectedNuclid.Unit);
+                var timePeriod = GetTimeDoubleValueInMinutes(residualActivityDate.DayNumber - initialActivityDate.DayNumber, "сут");
+                var cycles = timePeriod / halfLife;
+                if (cycles > 10000) cycles = 10000;
+                var activity = initialActivityDoubleValue / BigDecimal.Pow(2, cycles);
+                _activityCalculatorVM.ResidualActivity = ToExponentialString(activity);
+            }
+            else _activityCalculatorVM.ResidualActivity = string.Empty;
+        }
+        return Task.CompletedTask;
+    }
+
+    private static double GetTimeDoubleValueInMinutes(double halfLife, string unit)
+    {
+        return unit switch
+        {
+            "мин" => halfLife,
+            "час" => halfLife * 60,
+            "сут" => halfLife * 60 * 24,
+            "лет" => halfLife * 60 * 24 * 365,
+            _ => throw new ArgumentOutOfRangeException(nameof(unit), unit, null)
+        };
+    }
+
+    private protected static string ToExponentialString(object? value)
+    {
+        var tmp = (value?.ToString() ?? string.Empty)
+            .Trim()
+            .ToLower()
+            .Replace('е', 'e');
+        tmp = ReplaceDashes(tmp);
+        if (tmp != "прим.")
+        {
+            tmp = tmp.Replace('.', ',');
+        }
+        if (tmp is "прим." or "-")
+        {
+            return tmp;
+        }
+        var doubleStartsWithBrackets = false;
+        if (tmp.StartsWith('(') && tmp.EndsWith(')'))
+        {
+            doubleStartsWithBrackets = true;
+            tmp = tmp
+                .TrimStart('(')
+                .TrimEnd(')');
+        }
+        var tmpNumWithoutSign = tmp.StartsWith('+') || tmp.StartsWith('-')
+            ? tmp[1..]
+            : tmp;
+        var sign = tmp.StartsWith('-')
+            ? "-"
+            : string.Empty;
+        if (!tmp.Contains('e')
+            && tmpNumWithoutSign.Count(x => x is '+' or '-') == 1)
+        {
+            tmp = sign + tmpNumWithoutSign.Replace("+", "e+").Replace("-", "e-");
+        }
+        if (double.TryParse(tmp,
+                NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands | NumberStyles.AllowExponent | NumberStyles.AllowLeadingSign,
+                CultureInfo.CreateSpecificCulture("ru-RU"),
+                out var doubleValue))
+        {
+            tmp = $"{doubleValue:0.###e+00}";
+        }
+        return doubleStartsWithBrackets
+            ? $"({tmp})"
+            : tmp;
+    }
+
+    private protected static string ReplaceDashes(string value) =>
+        value switch
+        {
+            null => string.Empty,
+            _ => DashesRegex().Replace(value, "-")
+        };
+
+    [GeneratedRegex("[-᠆‐‑‒–—―⸺⸻－﹘﹣－]")]
+    protected static partial Regex DashesRegex();
+}
--- Client_App/ViewModels/Calculator/ActivityCalculatorVM.cs
+++ Client_App/ViewModels/Calculator/ActivityCalculatorVM.cs
@@ -1,15 +1,10 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Globalization;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text.RegularExpressions;
 using System.Windows.Input;
 using Client_App.Commands.AsyncCommands.Calculator;
-using ExtendedNumerics;
 
 namespace Client_App.ViewModels.Calculator;
 
@@ -42,7 +37,7 @@ public partial class ActivityCalculatorVM : BaseVM, INotifyPropertyChanged
             if (_selectedNuclid == value || value is null) return;
             _selectedNuclid = value;
             OnPropertyChanged();
-            OnPropertyChanged(nameof(ResidualActivity));
+            ActivityCalculation.Execute(this);
         }
     }
 
@@ -66,6 +61,7 @@ public partial class ActivityCalculatorVM : BaseVM, INotifyPropertyChanged
         {
             _isDateRange = value;
             OnPropertyChanged();
+            ActivityCalculation.Execute(this);
         }
     }
 
@@ -78,7 +74,8 @@ public partial class ActivityCalculatorVM : BaseVM, INotifyPropertyChanged
         set
         {
             _selectedTimeUnit = value;
-            OnPropertyChanged(nameof(ResidualActivity));
+            OnPropertyChanged();
+            ActivityCalculation.Execute(this);
         }
     }
 
@@ -90,27 +87,16 @@ public partial class ActivityCalculatorVM : BaseVM, INotifyPropertyChanged
         get => _initialActivity;
         set
         {
-            _initialActivity = ToExponentialString(value);
-            OnPropertyChanged(nameof(ResidualActivity));
+            _initialActivity = value;
+            OnPropertyChanged();
+            ActivityCalculation.Execute(this);
         }
     }
 
     private string _residualActivity;
     public string ResidualActivity
     {
-        get
-        {
-            if (double.TryParse(TimePeriodDouble, out var timePeriodDoubleValue) 
-                && double.TryParse(InitialActivity, out var initialActivityDoubleValue))
-            {
-                
-                var cycles = timePeriodDoubleValue / SelectedNuclid.Halflife;
-                var activity = initialActivityDoubleValue / BigDecimal.Pow(2, cycles);
-                var decimalValue = GetActivityInTargetType(activity, SelectedNuclid.Unit, SelectedTimeUnit);
-                return ToExponentialString(decimalValue);
-            }
-            return _residualActivity;
-        }
+        get => _residualActivity;
         set
         {
             _residualActivity = value;
@@ -125,82 +111,39 @@ public partial class ActivityCalculatorVM : BaseVM, INotifyPropertyChanged
         set
         {
             _timePeriodDouble = value;
-            OnPropertyChanged(nameof(ResidualActivity));
+            OnPropertyChanged();
+            ActivityCalculation.Execute(this);
         }
     }
 
-    private DateOnly _initialActivityDate = DateOnly.FromDateTime(DateTime.Now);
-    public DateOnly InitialActivityDate
+    private string _initialActivityDate;
+    public string InitialActivityDate
     {
         get => _initialActivityDate;
         set
         {
-            _initialActivityDate = value;
+            _initialActivityDate = DateTime.TryParse(value, out var dateTimeValue)
+                ? dateTimeValue.ToShortDateString()
+                : value;
+
             OnPropertyChanged();
+            ActivityCalculation.Execute(this);
         }
     }
 
-    private DateOnly _residualActivityDate = DateOnly.FromDateTime(DateTime.Now);
-    public DateOnly ResidualActivityDate
+    private string _residualActivityDate = DateTime.Now.ToShortDateString();
+    public string ResidualActivityDate
     {
         get => _residualActivityDate;
         set
         {
-            _residualActivityDate = value;
+            _residualActivityDate = DateTime.TryParse(value, out var dateTimeValue) 
+                ? dateTimeValue.ToShortDateString() 
+                : value;
+                
             OnPropertyChanged();
+            ActivityCalculation.Execute(this);
         }
-    }
-
-    private static BigDecimal GetActivityInTargetType(BigDecimal value, string currentUnit, string targetUnit)
-    {
-        switch (currentUnit)
-        {
-            case "мин":
-            {
-                switch (targetUnit)
-                {
-                    case "мин": return value;
-                    case "час": return value / 60;
-                    case "сут": return value / 60 / 24;
-                    case "лет": return value / 60 / 24 / 365;
-                }
-                break;
-            }
-            case "час":
-            {
-                switch (targetUnit)
-                {
-                    case "мин": return value * 60;
-                    case "час": return value;
-                    case "сут": return value / 24;
-                    case "лет": return value / 24 / 365;
-                }
-                break;
-            }
-            case "сут":
-            {
-                switch (targetUnit)
-                {
-                    case "мин": return value * 60 * 24;
-                    case "час": return value * 24;
-                    case "сут": return value;
-                    case "лет": return value / 365;
-                }
-                break;
-            }
-            case "лет":
-            {
-                switch (targetUnit)
-                {
-                    case "мин": return value * 60 * 24 * 365;
-                    case "час": return value * 24 * 365;
-                    case "сут": return value * 365;
-                    case "лет": return value;
-                }
-                break;
-            }
-        }
-        return value;
     }
 
     #region Constructor
@@ -213,67 +156,13 @@ public partial class ActivityCalculatorVM : BaseVM, INotifyPropertyChanged
         RadionuclidsFullList = [..Radionuclids];
 
         FilterCommand = new CalculatorFilterAsyncCommand(this);
+        ActivityCalculation = new ActivityCalculationAsyncCommand(this);
     }
 
     #endregion
 
-    public ICommand? FilterCommand { get; set; }
-
-    private protected static string ToExponentialString(object? value)
-    {
-        var tmp = (value?.ToString() ?? string.Empty)
-            .Trim()
-            .ToLower()
-            .Replace('е', 'e');
-        tmp = ReplaceDashes(tmp);
-        if (tmp != "прим.")
-        {
-            tmp = tmp.Replace('.', ',');
-        }
-        if (tmp is "прим." or "-")
-        {
-            return tmp;
-        }
-        var doubleStartsWithBrackets = false;
-        if (tmp.StartsWith('(') && tmp.EndsWith(')'))
-        {
-            doubleStartsWithBrackets = true;
-            tmp = tmp
-                .TrimStart('(')
-                .TrimEnd(')');
-        }
-        var tmpNumWithoutSign = tmp.StartsWith('+') || tmp.StartsWith('-')
-            ? tmp[1..]
-            : tmp;
-        var sign = tmp.StartsWith('-')
-            ? "-"
-            : string.Empty;
-        if (!tmp.Contains('e')
-            && tmpNumWithoutSign.Count(x => x is '+' or '-') == 1)
-        {
-            tmp = sign + tmpNumWithoutSign.Replace("+", "e+").Replace("-", "e-");
-        }
-        if (double.TryParse(tmp,
-                NumberStyles.AllowDecimalPoint | NumberStyles.AllowThousands | NumberStyles.AllowExponent | NumberStyles.AllowLeadingSign,
-                CultureInfo.CreateSpecificCulture("ru-RU"),
-                out var doubleValue))
-        {
-            tmp = $"{doubleValue:0.###e+00}";
-        }
-        return doubleStartsWithBrackets
-            ? $"({tmp})"
-            : tmp;
-    }
-
-    private protected static string ReplaceDashes(string value) =>
-        value switch
-        {
-            null => string.Empty,
-            _ => DashesRegex().Replace(value, "-")
-        };
-
-    [GeneratedRegex("[-᠆‐‑‒–—―⸺⸻－﹘﹣－]")]
-    protected static partial Regex DashesRegex();
+    public ICommand FilterCommand { get; set; }
+    public ICommand ActivityCalculation { get; set; }
 
     #region INotifyPropertyChanged
 
