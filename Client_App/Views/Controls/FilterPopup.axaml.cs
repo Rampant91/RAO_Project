162daabdb3745057b1a3eea0f380c2d8b63cc75c
BalaBoBik <115734734+BalaBoBik@users.noreply.github.com>
2025-09-11 10:39:21 +0300

    Start developing Filters (not working)

---
+++ Client_App/Views/Controls/FilterPopup.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Client_App.Controls;
+
+public partial class FilterPopup : UserControl
+{
+    public FilterPopup()
+    {
+        InitializeComponent();
+    }
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+
+    }
+}
