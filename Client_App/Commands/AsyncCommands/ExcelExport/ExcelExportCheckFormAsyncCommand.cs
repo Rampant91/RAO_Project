f87299dd7c77b801d3497ee2a1c6e8d088662972
Rampant91 <97747455+Rampant91@users.noreply.github.com>
2025-09-17 10:09:24 +0300

    fixes

--- Client_App/Commands/AsyncCommands/ExcelExport/ExcelExportCheckFormAsyncCommand.cs
+++ Client_App/Commands/AsyncCommands/ExcelExport/ExcelExportCheckFormAsyncCommand.cs
@@ -121,4 +121,77 @@ public class ExcelExportCheckFormAsyncCommand : ExcelBaseAsyncCommand
         Worksheet.View.FreezePanes(2, 1);
         return Task.CompletedTask;
     }
+
+    /// <summary>
+    /// Заполняет Excel пакет данными.
+    /// </summary>
+    /// <param name="checkFormVM">ViewModel окна проверки отчёта.</param>
+    /// <param name="progressBarVM">ViewModel прогрессбара.</param>
+    /// <returns>Успешно выполненная Task.</returns>
+    private Task FillExcel(NewCheckFormVM checkFormVM, AnyTaskProgressBarVM progressBarVM)
+    {
+        #region FillHeaders
+
+        Worksheet.Cells[1, 1].Value = "№ п/п";
+        Worksheet.Cells[1, 2].Value = "Стр.";
+        Worksheet.Cells[1, 3].Value = "Графа";
+        Worksheet.Cells[1, 4].Value = "Значение";
+        Worksheet.Cells[1, 5].Value = "Сообщение";
+
+        #endregion
+
+        #region FillData
+
+        var currentRow = 2;
+        double progressBarDoubleValue = progressBarVM.ValueBar;
+        foreach (var error in checkFormVM.CheckError)
+        {
+            Worksheet.Cells[currentRow, 1].Value = error.Index;
+            Worksheet.Cells[currentRow, 2].Value = error.Row;
+            Worksheet.Cells[currentRow, 3].Value = error.Column;
+            Worksheet.Cells[currentRow, 4].Value = error.Value;
+            Worksheet.Cells[currentRow, 5].Value = error.Message;
+
+            if (error.IsCritical)
+            {
+                Worksheet.Cells[currentRow, 1].Style.Fill.SetBackground(System.Drawing.Color.RosyBrown, ExcelFillStyle.LightGray);
+                Worksheet.Cells[currentRow, 2].Style.Fill.SetBackground(System.Drawing.Color.RosyBrown, ExcelFillStyle.LightGray);
+                Worksheet.Cells[currentRow, 3].Style.Fill.SetBackground(System.Drawing.Color.RosyBrown, ExcelFillStyle.LightGray);
+                Worksheet.Cells[currentRow, 4].Style.Fill.SetBackground(System.Drawing.Color.RosyBrown, ExcelFillStyle.LightGray);
+                Worksheet.Cells[currentRow, 5].Style.Fill.SetBackground(System.Drawing.Color.RosyBrown, ExcelFillStyle.LightGray);
+            }
+
+            currentRow++;
+
+            progressBarDoubleValue += (double)90 / checkFormVM.CheckError.Count;
+            progressBarVM.SetProgressBar((int)Math.Floor(double.Clamp(progressBarDoubleValue, 0, 100)), "Выгрузка ошибок");
+        }
+
+        #endregion
+
+        #region FormatCells
+
+        if (OperatingSystem.IsWindows())
+        {
+            for (var col = 1; col <= Worksheet.Dimension.End.Column; col++)
+            {
+                Worksheet.Column(col).AutoFit();
+                for (var row = 2; row < currentRow; row++)
+                {
+                    Worksheet.Cells[row, col].Style.WrapText = true;
+                    Worksheet.Cells[row, col].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Left;
+                    Worksheet.Cells[row, col].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Top;
+                }
+            }
+            Worksheet.Column(2).Width = 2 * Worksheet.Column(1).Width;
+            Worksheet.Column(3).Width = 2 * Worksheet.Column(1).Width;
+            Worksheet.Column(4).Width = 11 * Worksheet.Column(1).Width;
+            Worksheet.Column(5).Width = 11 * Worksheet.Column(1).Width;
+        }
+
+        #endregion
+
+        Worksheet.View.FreezePanes(2, 1);
+        return Task.CompletedTask;
+    }
 }
