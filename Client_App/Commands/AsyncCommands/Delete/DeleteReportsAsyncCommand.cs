415cea60060527b8799f4b9326f27705abd05bcd
Rampant91 <97747455+Rampant91@users.noreply.github.com>
2025-09-30 14:23:17 +0300

    teeeeeeeeeesst

--- Client_App/Commands/AsyncCommands/Delete/DeleteReportsAsyncCommand.cs
+++ Client_App/Commands/AsyncCommands/Delete/DeleteReportsAsyncCommand.cs
@@ -1,22 +1,25 @@
-﻿using System;
-using System.Collections;
-using System.Linq;
-using Models.Collections;
-using Models.DBRealization;
-using System.Threading.Tasks;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
+using Avalonia.Threading;
 using Client_App.Interfaces.Logger;
 using Client_App.Interfaces.Logger.EnumLogger;
 using MessageBox.Avalonia.DTO;
 using MessageBox.Avalonia.Models;
-using Avalonia.Threading;
+using Models.Collections;
+using Models.DBRealization;
+using Models.Forms;
+using Models.Forms.Form1;
+using Models.Forms.Form2;
+using System;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Client_App.Commands.AsyncCommands.Delete;
 
 /// <summary>
 /// Удалить выбранную организацию.
 /// </summary>
-internal class DeleteReportsAsyncCommand : BaseAsyncCommand
+public class DeleteReportsAsyncCommand : BaseAsyncCommand
 {
     public override async Task AsyncExecute(object? parameter)
     {
@@ -47,10 +50,12 @@ internal class DeleteReportsAsyncCommand : BaseAsyncCommand
             var enumerable = parameter as IEnumerable;
             var repsList = enumerable!.Cast<Reports>().ToList();
 
-            await using var db = new DBModel(StaticConfiguration.DBPath);
+            var db = StaticConfiguration.DBModel;
             db.ReportCollectionDbSet.Remove(repsList[0].Master_DB);
             db.ReportsCollectionDbSet.Remove(repsList[0]);
             await db.SaveChangesAsync();
+
+            await ProcessDataBaseFillEmpty(db);
         }
         catch (Exception ex)
         {
@@ -60,5 +65,42 @@ internal class DeleteReportsAsyncCommand : BaseAsyncCommand
         }
 
         //await Local_Reports.Reports_Collection.QuickSortAsync();
+    }
+
+    public static async Task ProcessDataBaseFillEmpty(DataContext dbm)
+    {
+        if (!dbm.DBObservableDbSet.Any()) dbm.DBObservableDbSet.Add(new DBObservable());
+        foreach (var item in dbm.DBObservableDbSet)
+        {
+            foreach (var key in item.Reports_Collection)
+            {
+                var it = (Reports)key;
+                if (it.Master_DB.FormNum_DB == "") continue;
+                if (it.Master_DB.Rows10.Count == 0)
+                {
+                    var ty1 = (Form10)FormCreator.Create("1.0");
+                    ty1.NumberInOrder_DB = 1;
+                    var ty2 = (Form10)FormCreator.Create("1.0");
+                    ty2.NumberInOrder_DB = 2;
+                    it.Master_DB.Rows10.Add(ty1);
+                    it.Master_DB.Rows10.Add(ty2);
+                }
+
+                if (it.Master_DB.Rows20.Count == 0)
+                {
+                    var ty1 = (Form20)FormCreator.Create("2.0");
+                    ty1.NumberInOrder_DB = 1;
+                    var ty2 = (Form20)FormCreator.Create("2.0");
+                    ty2.NumberInOrder_DB = 2;
+                    it.Master_DB.Rows20.Add(ty1);
+                    it.Master_DB.Rows20.Add(ty2);
+                }
+
+                it.Master_DB.Rows10.Sorted = false;
+                it.Master_DB.Rows20.Sorted = false;
+                await it.Master_DB.Rows10.QuickSortAsync();
+                await it.Master_DB.Rows20.QuickSortAsync();
+            }
+        }
     }
 }
