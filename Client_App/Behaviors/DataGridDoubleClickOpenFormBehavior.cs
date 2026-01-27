1bd1a9ab7cf15d9a84afac9502ef303c9ae41702
BalaBoBik <115734734+BalaBoBik@users.noreply.github.com>
2026-01-27 09:05:33 +0300

    Add DoubleClick in Forms4TabControl

---
+++ Client_App/Behaviors/DataGridDoubleClickOpenFormBehavior.cs
@@ -0,0 +1,60 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Xaml.Interactivity;
+using AvaloniaEdit.Utils;
+using Client_App.Commands.AsyncCommands;
+using Models.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client_App.Behaviors
+{
+    public class DataGridDoubleClickOpenFormBehavior : Behavior<DataGrid>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            if (AssociatedObject != null)
+            {
+                // Подписываемся на события
+                AssociatedObject.DoubleTapped += DataGrid_DoubleTapped; ;
+            }
+        }
+
+
+        protected override void OnDetaching()
+        {
+            if (AssociatedObject != null)
+            {
+                AssociatedObject.DoubleTapped -= DataGrid_DoubleTapped;
+            }
+
+            base.OnDetaching();
+        }
+
+        private void DataGrid_DoubleTapped(object? sender, RoutedEventArgs e)
+        {
+            if (AssociatedObject?.SelectedItem != null)
+            {
+                if(AssociatedObject?.SelectedItem is Reports reports)
+                {
+                    var command = new NewChangeReportsAsyncCommand();
+                    command.AsyncExecute(reports);
+                }
+                else if (AssociatedObject?.SelectedItem is Report report)
+                {
+                    var command = new NewChangeFormAsyncCommand();
+                    command.AsyncExecute(report);
+                }
+            }
+
+        }
+    }
+
+}
