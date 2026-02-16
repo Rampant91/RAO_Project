68ee885143c0465d46368eb1a9f228bcdc8c3b65
BalaBoBik <115734734+BalaBoBik@users.noreply.github.com>
2026-02-16 11:56:38 +0300

    Update PackagePassport.cs

--- Models/Passports/PackagePassport.cs
+++ Models/Passports/PackagePassport.cs
@@ -11,97 +11,76 @@ namespace Models.Passports
     {
         string PassportNum;
         DateOnly PassportDate;
-
         string PackageType;
 
         byte StatusRaoCode;
-
         string TechSpecificationNum;
-
         string NameRao;
         byte ClassRao;
 
         string AgreementNumRaoDisposal;
         string UniquePackageNum;
-
         string TypePuod;
         string IdPuod;
-
         string OwnerRaoPackage;
         string ManufacturerRaoPackage;
         string CertificateConformityNum;
         DateOnly StartPeriodCertificateConformity;
         DateOnly EndPeriodCertificateConformity;
         int ServiceLife;
-
         DateOnly ManufactureDate;
         DateOnly TransferDate;
 
         #region Общая характеристика упаковки РАО
 
         string DisposalMethod;
-
-        string PrimaryPackageType;
-        string PrimaryPackageNum;
-        int PrimaryPackageQuantity;
-        double PrimaryPackageVolume;
-        double PrimaryPackageMass;
-
+        string PhysicochemicalForm;
+        string MorphologicalCompositionRao;
+        string Flammability;
         string? MatrixMaterialType;
-        DateOnly FillingWasteDate;
-        
-
+        DateOnly? FillingWasteDate;
         double Diameter;
         double Height;
         double Lenght;
         double Width;
-
         double PackageMass;
         double RaoMass;
-
         double RadiationDoseRate1m;
         double RadiationDoseRate10cm;
-
         double LevelNonFixedPollutionAlpha;
         double LevelNonFixedPollutionBetaGamma;
-
         double? PackageHeatOutput;
         #endregion
 
         #region Характеристика радиоактивного содержимого упаковки РАО
 
-        //string string UniquePackageNum;
-
-        //byte ClassRao;
-
-        string CodeRao;
-
-        string PhysicochemicalForm;
-
-        string MorphologicalCompositionRao;
-
-        string Flammability;
-
-        string RadionuclidComposition;
-
-        double Activity;
-
-        double LongLivingActivity;
-        double TransuraniumActivity;
-        double AlphaActivity;
-        double BetaGammaActivity;
-        double TritiumActivity;
-
-        double TotalActivity;
-
-        string? NuclearHazardousFissileNuclides;
+        List<CharacteristicOfContentRaoPackage> ContentCharacteristics = new List<CharacteristicOfContentRaoPackage>();
 
         #endregion
 
         string ResponsibleForTransferRao;
         string GradeFioAuthotizedPersonTransfer;
-
         string ResponsibleForReceptionRao;
         string GradeFioAuthotizedPersonReception;
+    }
+
+    public class CharacteristicOfContentRaoPackage
+    {
+        string PrimaryPackageType;
+        string PrimaryPackageNum;
+        byte ClassRao;
+        string CodeRao;
+        int PrimaryPackageQuantity;
+        double PrimaryPackageVolume;
+        double PrimaryPackageMass;
+        string RadionuclidComposition;
+        double Activity;
+        double LongLivingActivity;
+        double TransuraniumActivity;
+        double AlphaActivity;
+        double BetaGammaActivity;
+        double TritiumActivity;
+        double TotalActivity;
+        string? NuclearHazardousFissileNuclides;
     }
 }
