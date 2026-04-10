b0f883178f2c856ae038c36d37206832d178546a
Rampant91 <97747455+Rampant91@users.noreply.github.com>
2026-04-10 11:41:02 +0300

    Update description

--- Client_App/ViewModels/Forms/Forms1/OperationCodesProvider.cs
+++ Client_App/ViewModels/Forms/Forms1/OperationCodesProvider.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Security.Cryptography.Pkcs;
 
 namespace Client_App.ViewModels.Forms.Forms1;
 
@@ -14,73 +16,149 @@ public static class OperationCodesProvider
     /// </summary>
     public static ObservableCollection<OperationCodeItem> AllOperationCodes { get; } =
     [
-        new() { Code = "01", Description = "Код 01" },
-        new() { Code = "10", Description = "Код 10" },
-        new() { Code = "11", Description = "Код 11" },
-        new() { Code = "12", Description = "Код 12" },
-        new() { Code = "13", Description = "Код 13" },
-        new() { Code = "14", Description = "Код 14" },
-        new() { Code = "15", Description = "Код 15" },
-        new() { Code = "16", Description = "Код 16" },
-        new() { Code = "17", Description = "Код 17" },
-        new() { Code = "18", Description = "Код 18" },
-        new() { Code = "21", Description = "Код 21" },
-        new() { Code = "22", Description = "Код 22" },
-        new() { Code = "25", Description = "Код 25" },
-        new() { Code = "26", Description = "Код 26" },
-        new() { Code = "27", Description = "Код 27" },
-        new() { Code = "28", Description = "Код 28" },
-        new() { Code = "29", Description = "Код 29" },
-        new() { Code = "31", Description = "Код 31" },
-        new() { Code = "32", Description = "Код 32" },
-        new() { Code = "35", Description = "Код 35" },
-        new() { Code = "36", Description = "Код 36" },
-        new() { Code = "37", Description = "Код 37" },
-        new() { Code = "38", Description = "Код 38" },
-        new() { Code = "39", Description = "Код 39" },
-        new() { Code = "41", Description = "Код 41" },
-        new() { Code = "42", Description = "Код 42" },
-        new() { Code = "43", Description = "Код 43" },
-        new() { Code = "44", Description = "Код 44" },
-        new() { Code = "45", Description = "Код 45" },
-        new() { Code = "46", Description = "Код 46" },
-        new() { Code = "47", Description = "Код 47" },
-        new() { Code = "48", Description = "Код 48" },
-        new() { Code = "49", Description = "Код 49" },
-        new() { Code = "51", Description = "Код 51" },
-        new() { Code = "52", Description = "Код 52" },
-        new() { Code = "53", Description = "Код 53" },
-        new() { Code = "54", Description = "Код 54" },
-        new() { Code = "55", Description = "Код 55" },
-        new() { Code = "56", Description = "Код 56" },
-        new() { Code = "57", Description = "Код 57" },
-        new() { Code = "58", Description = "Код 58" },
-        new() { Code = "59", Description = "Код 59" },
-        new() { Code = "61", Description = "Код 61" },
-        new() { Code = "62", Description = "Код 62" },
-        new() { Code = "63", Description = "Код 63" },
-        new() { Code = "64", Description = "Код 64" },
-        new() { Code = "65", Description = "Код 65" },
-        new() { Code = "66", Description = "Код 66" },
-        new() { Code = "67", Description = "Код 67" },
-        new() { Code = "68", Description = "Код 68" },
-        new() { Code = "71", Description = "Код 71" },
-        new() { Code = "72", Description = "Код 72" },
-        new() { Code = "73", Description = "Код 73" },
-        new() { Code = "74", Description = "Код 74" },
-        new() { Code = "75", Description = "Код 75" },
-        new() { Code = "76", Description = "Код 76" },
-        new() { Code = "81", Description = "Код 81" },
-        new() { Code = "82", Description = "Код 82" },
-        new() { Code = "83", Description = "Код 83" },
-        new() { Code = "84", Description = "Код 84" },
-        new() { Code = "85", Description = "Код 85" },
-        new() { Code = "86", Description = "Код 86" },
-        new() { Code = "87", Description = "Код 87" },
-        new() { Code = "88", Description = "Код 88" },
-        new() { Code = "97", Description = "Код 97" },
-        new() { Code = "98", Description = "Код 98" },
-        new() { Code = "99", Description = "Код 99" }
+        new() { Code = "01", Description = "Наличие на дату 31.12.2021" },
+
+        new() { Code = "10", Description = "Инвентаризация" },
+
+        new() { Code = "11", Description = "Изготовление / образование" },
+
+        new() { Code = "12", Description = "Перевод из ЯМ в РВ и РАО" },
+
+        new() { Code = "13", Description = "Образование РАО при обслуживании пунктов хранения/захоронения РАО" },
+
+        new() { Code = "14", Description = "Постановка на учёт РАО, образовавшихся из РВ или не подлежащих учёту" },
+
+        new() { Code = "15", Description = "Возврат в оборот РВ при переработке ОЗИИИ" },
+
+        new() { Code = "16", Description = "Постановка на учёт РАО, образовавшихся при выводе из эксплуатации ОИАЭ " +
+                                           "или реабилитации загрязнённых территорий" },
+
+        new() { Code = "17", Description = "Постановка на учёт РВ, ранее не подлежавших учёту" },
+
+        new() { Code = "18", Description = "Постановка на учёт при изменении характеристик по результатам измерений" },
+
+        new() { Code = "21", Description = "Передача одним об. подразделением другому об. подразделению одного юр. лица" },
+
+        new() { Code = "22", Description = "Передача в Министерство обороны РФ" },
+
+        new() { Code = "25", Description = "Передача (возврат) ранее полученного от юр. лица РФ" },
+
+        new() { Code = "26", Description = "Передача юр. лицу РФ с обязательствами по обеспечению безопасности " +
+                                           "до передачи нац. оператору" },
+
+        new() { Code = "27", Description = "Передача юр. лицу РФ без передачи права собственности" },
+
+        new() { Code = "28", Description = "Передача юр. лицу РФ с передачей права собственности" },
+
+        new() { Code = "29", Description = "Прочие операции по передаче" },
+
+        new() { Code = "31", Description = "Получение одним об. подразделением от другого " +
+                                           "об. подразделения того же юр. лица" },
+
+        new() { Code = "32", Description = "Получение из Министерство обороны РФ" },
+
+        new() { Code = "35", Description = "Получение от юр. лица РФ, без перехода права собственности " +
+                                           "(иного вещного права)" },
+
+        new() { Code = "36", Description = "Получение от юр. лица, с обязательствами по обеспечению безопасности " +
+                                           "до передачи нац. оператору" },
+
+        new() { Code = "37", Description = "Получение (возврат) ранее переданного юр. лицу РФ" },
+
+        new() { Code = "38", Description = "Получение учётной единицы и права собственности на неё" },
+
+        new() { Code = "39", Description = "Прочие операции по получению" },
+
+        new() { Code = "41", Description = "Перевод РВ в РАО" },
+
+        new() { Code = "42", Description = "Перевод из РВ и РАО в ЯМ" },
+
+        new() { Code = "43", Description = "Снятие учётной единицы по причине естественного распада" },
+
+        new() { Code = "44", Description = "Переработка, кондиционирование" },
+
+        new() { Code = "45", Description = "Снятие с учёта РАО при операциях упаковки, переупаковки" },
+
+        new() { Code = "46", Description = "Использование (разукомплектование, расфасовка) для изготовления, " +
+                                           "образования учётных единиц, подлежащих учёту" },
+
+        new() { Code = "47", Description = "Расходование" },
+
+        new() { Code = "48", Description = "Безвозвратные потери в пределах установленных норм" },
+
+        new() { Code = "49", Description = "Снятие с учёта РАО при операциях сортировки" },
+
+        new() { Code = "51", Description = "Изъятие из пункта хранения" },
+
+        new() { Code = "52", Description = "Размещение в пункте хранения" },
+
+        new() { Code = "53", Description = "Зарядка (загрузка) / разрядка (выгрузка) организацией" },
+
+        new() { Code = "54", Description = "Зарядка (загрузка) / разрядка (выгрузка) подрядной организацией" },
+
+        new() { Code = "55", Description = "Приведение в соответствие критериям приемлемости РАО для захоронения" },
+
+        new() { Code = "56", Description = "Образование РАО после переработки" },
+
+        new() { Code = "57", Description = "Постановка на учёт РАО при операциях упаковки или переупаковки" },
+
+        new() { Code = "58", Description = "Вторичное образование РВ" },
+
+        new() { Code = "59", Description = "Постановка на учёт РАО при операциях сортировки" },
+
+        new() { Code = "61", Description = "Вывоз на другую территорию" },
+
+        new() { Code = "62", Description = "Возврат с другой территории" },
+
+        new() { Code = "63", Description = "Передача права собственности (иного вещного права), " +
+                                           "обязательств по оплате захоронения, без физического перемещения" },
+
+        new() { Code = "64", Description = "Получение права собственности (иного вещного права), " +
+                                           "обязательств по передаче РАО на захоронение, без физического перемещения" },
+
+        new() { Code = "65", Description = "Перевод из ЗРИ в ОРИ" },
+
+        new() { Code = "66", Description = "Продление ИСС ЗРИ/ИОУ" },
+
+        new() { Code = "67", Description = "Перевод в РВ при проведении работ по использованию атомной энергии " +
+                                           "в оборонных целях" },
+
+        new() { Code = "68", Description = "Снятие с учёта при изменении характеристик по результатам измерений" },
+
+        new() { Code = "71", Description = "Утеря" },
+
+        new() { Code = "72", Description = "Утрата контроля при известном местоположении" },
+
+        new() { Code = "73", Description = "Обнаружение неучтённого" },
+
+        new() { Code = "74", Description = "Изъятие из незаконного оборота" },
+
+        new() { Code = "75", Description = "Обнаружение утерянного ранее" },
+
+        new() { Code = "76", Description = "Постановка на учёт РАО, изымаемых по указанию " +
+                                           "органов исполнительной власти, надзорных органов" },
+
+        new() { Code = "81", Description = "Экспорт" },
+
+        new() { Code = "82", Description = "Временный вывоз из РФ" },
+
+        new() { Code = "83", Description = "Вывоз (возврат) из РФ временно ввезённого ранее" },
+
+        new() { Code = "84", Description = "Вывоз (возврат) ранее импортированного ЗРИ (ОЗИИИ) за рубеж" },
+
+        new() { Code = "85", Description = "Импорт" },
+
+        new() { Code = "86", Description = "Временный ввоз в РФ" },
+
+        new() { Code = "87", Description = "Возврат (ввоз) в РФ временно вывезенного ранее" },
+
+        new() { Code = "88", Description = "Ввоз (возврат) в РФ ранее экспортированного ЗРИ (ОЗИИИ)" },
+
+        new() { Code = "97", Description = "Постановка на учёт по другим причинам" },
+
+        new() { Code = "98", Description = "Снятие с учёта по другим причинам" },
+
+        new() { Code = "99", Description = "Прочие операции" }
     ];
 
     /// <summary>
