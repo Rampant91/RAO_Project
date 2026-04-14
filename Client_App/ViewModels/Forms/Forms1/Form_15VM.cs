837bc18f18b2693a9e9bcb5b56691211e7eca345
Rampant91 <97747455+Rampant91@users.noreply.github.com>
2026-04-14 10:08:26 +0300

    Add RefineOrSortRAO AutoCompleteBox in 1.5

--- Client_App/ViewModels/Forms/Forms1/Form_15VM.cs
+++ Client_App/ViewModels/Forms/Forms1/Form_15VM.cs
@@ -32,6 +32,22 @@ public class Form_15VM : BaseFormVM
     /// </summary>
     public ICollection<string> ValidDocumentVids => DocumentVidProvider.GetValidCodes().ToList();
 
+    /// <summary>
+    /// 
+    /// </summary>
+    public ICollection<string> ValidOwnershipForms => OwnershipProvider.GetValidCodes().ToList();
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public ObservableCollection<RefineOrSortRAOCodeItem> RefineOrSortRAOCodes =>
+        new(RefineOrSortRAOCodeProvider.AllRefineOrSortRAOCodes);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public ICollection<string> ValidRefineOrSortRAOCodes => RefineOrSortRAOCodeProvider.GetValidCodes().ToList();
+
     #region Constructors
 
     public Form_15VM() { }
