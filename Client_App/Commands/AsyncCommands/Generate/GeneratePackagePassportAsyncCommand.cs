ef1adfbe17203a6d75ecbdd526b5a9cd278fbe55
BalaBoBik <115734734+BalaBoBik@users.noreply.github.com>
2026-04-15 17:00:52 +0300

    Update GeneratePackagePassportAsyncCommand.cs

--- Client_App/Commands/AsyncCommands/Generate/GeneratePackagePassportAsyncCommand.cs
+++ Client_App/Commands/AsyncCommands/Generate/GeneratePackagePassportAsyncCommand.cs
@@ -4,6 +4,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Threading;
 using Client_App.ViewModels.Forms;
 using Client_App.Views;
+using DynamicData;
 using MessageBox.Avalonia.DTO;
 using MessageBox.Avalonia.Models;
 using Models.Collections;
@@ -18,6 +19,7 @@ using System.Collections.ObjectModel;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using static Models.Collections.Report;
@@ -27,18 +29,58 @@ namespace Client_App.Commands.AsyncCommands.Generate
     public class GeneratePackagePassportAsyncCommand(BaseFormVM formVM) : BaseAsyncCommand
     {
         Report Report => formVM.Report;
+        List<Form17> Rows17
+        {
+            get
+            {
+                return Report.Rows17.OrderBy(form17 => form17.NumberInOrder_DB).ToList();
+            }
+        }
         Window owner => (Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.Windows
                     .FirstOrDefault(w => w.Name == "1.7");
 
         //На вход поступает строки формы 1.7
         public override async Task AsyncExecute(object? parameter)
         {
-            if (parameter is not IEnumerable<Form> forms17Collection) return;
+            if (parameter is not IEnumerable<Form> forms17Collection
+                || forms17Collection.Count()<=0
+                || forms17Collection.Any(f => f is not Form17)) return;
+
+            var codeOperationRegex = new Regex("^\\d{2}$");
+
+            var selectedForm17List = forms17Collection.Cast<Form17>().ToList();
+
+
+            var first = selectedForm17List.First();
+            var firstIndex = Rows17.IndexOf(first);
+
+            while (firstIndex > 0
+                && !codeOperationRegex.IsMatch(first.OperationCode_DB))
+            {
+                selectedForm17List.Insert(0, Rows17[firstIndex - 1]);
+                first = Rows17[firstIndex - 1];
+                firstIndex--;
+            }
+
+
+            var last = selectedForm17List.Last();
+            var lastIndex = Rows17.IndexOf(last);
+            while (lastIndex < Rows17.Count-1
+                && !codeOperationRegex.IsMatch(Rows17[lastIndex + 1].OperationCode_DB))
+            {
+                selectedForm17List.Add(Rows17[lastIndex + 1]); 
+                last = Rows17[lastIndex + 1];
+                lastIndex++;
+            }
+
 
             //ссылка на нужный список радионуклидов
             ObservableCollection<Radionuclid>? radionuclidList = null;
 
-            foreach (var form in forms17Collection)
+            // Список строк, для которых не будет создан паспорт, т.к. их код операции не предполагает создания паспорта
+            List<int> rowNumbers = new();
+
+            foreach (var form in selectedForm17List)
             {
                 if (form is not Form17 form17) continue;
 
@@ -49,8 +91,7 @@ namespace Client_App.Commands.AsyncCommands.Generate
                 //Проверка строки новая ли это запись или продолжение старой 
                 //Если новая то создаем новый паспорт и добавляем радионуклид
                 //Если продолжение старой то только добавляем радионуклид 
-                if (!string.IsNullOrWhiteSpace(form17.PackType_DB)
-                    && !string.IsNullOrWhiteSpace(form17.PassportNumber_DB)) 
+                if (codeOperationRegex.IsMatch(form17.OperationCode_DB)) 
                 {
                     var passportMatch = FindPassportMatch(form17);
 
@@ -62,6 +103,7 @@ namespace Client_App.Commands.AsyncCommands.Generate
                         && form17.OperationCode_DB != "18"
                         && form17.OperationCode_DB != "55")
                     {
+                        rowNumbers.Add(form17.NumberInOrder_DB);
                         radionuclidList = null;
                         continue;
                     }
@@ -125,6 +167,35 @@ namespace Client_App.Commands.AsyncCommands.Generate
                 }
 
             }
+            if (rowNumbers.Count > 0)
+            {
+                var msg = $"Не удалось создать паспорт на основе данных строк:\n" +
+                    $"№";
+                for(int i =0; i < rowNumbers.Count; i++)
+                {
+                    if (i != rowNumbers.Count - 1)
+                        msg += $" {rowNumbers[i]}, ";
+                    else
+                        msg += $" {rowNumbers[i]}\n";
+                }
+                msg += "Так как их код операции не равен 01, 11, 12, 14, 16, 18, 55";
+
+                await Dispatcher.UIThread.InvokeAsync(() => MessageBox.Avalonia.MessageBoxManager
+                .GetMessageBoxCustomWindow(new MessageBoxCustomParams()
+                {
+                    ButtonDefinitions =
+                    [
+                        new ButtonDefinition { Name = "Ок" },
+                    ],
+                    CanResize = true,
+                    ContentTitle = "Формирование паспорта на упаковку",
+                    ContentMessage = msg,
+                    MinWidth = 300,
+                    MinHeight = 125,
+                    WindowStartupLocation = WindowStartupLocation.CenterOwner
+                })
+                .ShowDialog(owner));
+            }
 
             #region DBModel.SaveChanges
             try
@@ -133,7 +204,7 @@ namespace Client_App.Commands.AsyncCommands.Generate
             }
             catch (Exception ex)
             {
-                Dispatcher.UIThread.InvokeAsync(() => MessageBox.Avalonia.MessageBoxManager
+                await Dispatcher.UIThread.InvokeAsync(() => MessageBox.Avalonia.MessageBoxManager
                 .GetMessageBoxCustomWindow(new MessageBoxCustomParams
                 {
                     ButtonDefinitions =
