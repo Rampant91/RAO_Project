c150177d054c76260c81486114d1ce00a9ab122b
Rampant91 <97747455+Rampant91@users.noreply.github.com>
2026-05-14 10:43:10 +0300

    fix bug with multibuinding in FormNumButton Control

--- Client_App/VisualRealization/Converters/CheckReportsAnyMatchingFormNumConverter.cs
+++ Client_App/VisualRealization/Converters/CheckReportsAnyMatchingFormNumConverter.cs
@@ -1,6 +1,8 @@
-﻿using Avalonia.Data.Converters;
+﻿using Avalonia;
+using Avalonia.Data.Converters;
 using Models.Collections;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 
@@ -8,7 +10,7 @@ namespace Client_App.VisualRealization.Converters;
 
 //Этот конвертер используется, чтобы деактивировать кнопки белого списка в FormsTabControl.
 //Настроить CanExecute у команды не получилось, т.к. нужно его обновлять после каждого изменения SelectedReports 
-public class CheckReportsAnyMatchingFormNumConverter : IValueConverter
+public class CheckReportsAnyMatchingFormNumConverter : IValueConverter, IMultiValueConverter
 {
     public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
     {
@@ -22,5 +24,19 @@ public class CheckReportsAnyMatchingFormNumConverter : IValueConverter
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
     {
         throw new NotImplementedException();
+    }
+
+    public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values is null || values.Count < 2)
+            return false;
+
+        if (values[0] == AvaloniaProperty.UnsetValue || values[1] == AvaloniaProperty.UnsetValue)
+            return false;
+
+        if (values[0] is Reports selectedReports && values[1] is string formNum)
+            return selectedReports.Report_Collection.Any(rep => rep.FormNum_DB == formNum);
+
+        return false;
     }
 }
--- Client_App/VisualRealization/Converters/StringEqualsToColorConverter.cs
+++ Client_App/VisualRealization/Converters/StringEqualsToColorConverter.cs
@@ -1,11 +1,13 @@
-﻿using Avalonia.Data.Converters;
+﻿using Avalonia;
+using Avalonia.Data.Converters;
 using Avalonia.Media;
 using System;
+using System.Collections.Generic;
 using System.Globalization;
 
 namespace Client_App.VisualRealization.Converters;
 
-public class StringEqualsToColorConverter : IValueConverter
+public class StringEqualsToColorConverter : IValueConverter, IMultiValueConverter
 {
     public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
@@ -23,5 +25,24 @@ public class StringEqualsToColorConverter : IValueConverter
     public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
         throw new NotSupportedException();
+    }
+
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values is null || values.Count < 2)
+            return Brushes.WhiteSmoke;
+
+        if (values[0] == AvaloniaProperty.UnsetValue || values[1] == AvaloniaProperty.UnsetValue)
+            return Brushes.WhiteSmoke;
+
+        string? firstString = values[0]?.ToString();
+        string? secondString = values[1]?.ToString();
+
+        if (firstString == null || secondString == null)
+            return Brushes.WhiteSmoke;
+
+        bool areEqual = string.Equals(firstString, secondString);
+
+        return areEqual ? Brushes.LightGreen : Brushes.WhiteSmoke;
     }
 }
