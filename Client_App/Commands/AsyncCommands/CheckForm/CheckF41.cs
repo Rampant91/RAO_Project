723f81f27f9f3f81f627df4f27b61f61100aabf0
BalaBoBik <115734734+BalaBoBik@users.noreply.github.com>
2026-05-22 10:37:12 +0300

    Update CheckF41.cs
    Исправил запрос в проверках форм 4.1
    
    По неизвестным причинам у некоторых организаций не совпадает РегНомер у ЮрЛица и обособленого подразделения. Из-за это организация с Регномером 66135

--- Client_App/Commands/AsyncCommands/CheckForm/CheckF41.cs
+++ Client_App/Commands/AsyncCommands/CheckForm/CheckF41.cs
@@ -67,21 +67,26 @@ public abstract class CheckF41 : CheckBase
             foreach (var reports in reportsQuery)
             {
                 cts.Token.ThrowIfCancellationRequested();
-                    
-
                 if (formList.Any(form41 =>
-                        form41.RegNo_DB == reports.Master_DB.Rows10[0].RegNo_DB
-                        && (form41.Okpo_DB == reports.Master_DB.Rows10[0].Okpo_DB
-                            || form41.Okpo_DB == reports.Master_DB.Rows10[1].Okpo_DB)))
+                    (form41.RegNo_DB == reports.Master_DB.Rows10[0].RegNo_DB
+                    || form41.RegNo_DB == reports.Master_DB.Rows10[1].RegNo_DB )
+                    && (form41.Okpo_DB == reports.Master_DB.Rows10[0].Okpo_DB
+                    || form41.Okpo_DB == reports.Master_DB.Rows10[1].Okpo_DB)))
                 {
-                    var okpo = reports.Master_DB.Rows10[1].Okpo_DB is "" or "-" or null 
-                        ? reports.Master_DB.Rows10[0].Okpo_DB 
+                    var okpo = reports.Master_DB.Rows10[1].Okpo_DB is "" or "-" or null
+                        ? reports.Master_DB.Rows10[0].Okpo_DB
                         : reports.Master_DB.Rows10[1].Okpo_DB;
+
+                    string regNo;
+                    if ((reports.Master_DB.Rows10[1].RegNo.Value != "" || reports.Master_DB.Rows10[1].Okpo_DB == "-") && reports.Master_DB.Rows10[1].Okpo.Value != "")
+                        regNo = reports.Master_DB.Rows10[1].RegNo_DB;
+                    else
+                        regNo = reports.Master_DB.Rows10[0].RegNo_DB;
 
                     organizations10.Add(new Organization()
                     {
                         Id = reports.Id,
-                        RegNo = reports.Master_DB.Rows10[0].RegNo_DB,
+                        RegNo = regNo,
                         Okpo = okpo,
                     });
                 }
@@ -159,18 +164,25 @@ public abstract class CheckF41 : CheckBase
                 cts.Token.ThrowIfCancellationRequested();
 
                 if (formList.Any(form41 =>
-                        form41.RegNo_DB == reports.Master_DB.Rows20[0].RegNo_DB
-                        && (form41.Okpo_DB == reports.Master_DB.Rows20[0].Okpo_DB
-                            || form41.Okpo_DB == reports.Master_DB.Rows20[1].Okpo_DB)))
+                     (form41.RegNo_DB == reports.Master_DB.Rows20[0].RegNo_DB
+                     || form41.RegNo_DB == reports.Master_DB.Rows20[1].RegNo_DB)
+                     && (form41.Okpo_DB == reports.Master_DB.Rows20[0].Okpo_DB
+                     || form41.Okpo_DB == reports.Master_DB.Rows20[1].Okpo_DB)))
                 {
-                    var okpo = reports.Master_DB.Rows20[1].Okpo_DB is "" or "-" or null 
-                        ? reports.Master_DB.Rows20[0].Okpo_DB 
+                    var okpo = reports.Master_DB.Rows20[1].Okpo_DB is "" or "-" or null
+                        ? reports.Master_DB.Rows20[0].Okpo_DB
                         : reports.Master_DB.Rows20[1].Okpo_DB;
+
+                    string regNo;
+                    if ((reports.Master_DB.Rows20[1].RegNo.Value != "" || reports.Master_DB.Rows20[1].Okpo_DB == "-") && reports.Master_DB.Rows20[1].Okpo.Value != "")
+                        regNo = reports.Master_DB.Rows20[1].RegNo_DB;
+                    else
+                        regNo = reports.Master_DB.Rows20[0].RegNo_DB;
 
                     organizations20.Add(new Organization()
                     {
                         Id = reports.Id,
-                        RegNo = reports.Master_DB.Rows20[0].RegNo_DB,
+                        RegNo = regNo,
                         Okpo = okpo,
                     });
                 }
@@ -555,6 +567,7 @@ public abstract class CheckF41 : CheckBase
         cts.Token.ThrowIfCancellationRequested();
 
         if (!int.TryParse(form41.Report.Year_DB, out var year)) return null;
+
 
         var organization = organizations10.FirstOrDefault(org => 
             org.RegNo == form41.RegNo_DB && org.Okpo == form41.Okpo_DB);
@@ -1046,7 +1059,7 @@ public abstract class CheckF41 : CheckBase
         if (!inventoryFlag && (massBalanceLiquid > 0 || massBalanceSolid > 0 || massBalanceGas > 0))
             return new CheckError
             {
-                Row = "{form41.NumberInOrder_DB}",
+                Row = $"{form41.NumberInOrder_DB}",
                 RegNo = form41.RegNo_DB,
                 Okpo = form41.Okpo_DB,
                 DbValue = $"{massBalanceLiquid}" +
