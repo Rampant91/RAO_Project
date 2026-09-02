87e19d218c67cdd78c38adf23139f1afe6f6fb4b
BalaBoBik <115734734+BalaBoBik@users.noreply.github.com>
2026-09-02 09:17:53 +0300

    Update ReorganizeReportAsyncCommand.cs

--- Client_App/Commands/AsyncCommands/ReorganizeReportAsyncCommand.cs
+++ Client_App/Commands/AsyncCommands/ReorganizeReportAsyncCommand.cs
@@ -7,6 +7,7 @@ using MessageBox.Avalonia.DTO;
 using MessageBox.Avalonia.Models;
 using Avalonia.Controls;
 using System.Threading.Tasks;
+using Client_App.ViewModels.Forms.Forms3;
 
 namespace Client_App.Commands.AsyncCommands;
 
@@ -27,6 +28,10 @@ public class ReorganizeReportAsyncCommand : BaseAsyncCommand
         else if (vm is Form_20VM form20VM)
         {
             _vm = form20VM;
+        }
+        else if (vm is Form_30VM form30VM)
+        {
+            _vm = form30VM;
         }
     }
 
@@ -96,7 +101,7 @@ public class ReorganizeReportAsyncCommand : BaseAsyncCommand
             }
             form_10VM.IsSeparateDivision = !form_10VM.IsSeparateDivision;
         }
-        if (_vm is Form_20VM form_20VM)
+        else if (_vm is Form_20VM form_20VM)
         {
 
             //  Если подразделение реорганизуется в юр. лицо,
@@ -157,6 +162,68 @@ public class ReorganizeReportAsyncCommand : BaseAsyncCommand
                 //реорганизация формы
             }
             form_20VM.IsSeparateDivision = !form_20VM.IsSeparateDivision;
+        }
+        else if (_vm is Form_30VM form_30VM)
+        {
+
+            //  Если подразделение реорганизуется в юр. лицо,
+            // поля Территориального обособленного подразделения очищаются,
+            // т.к. после реорганизации они не должны больше использоваться
+
+            if (form_30VM.IsSeparateDivision)
+            {
+                // Предупреждение пользователя
+                var answer = await Dispatcher.UIThread.InvokeAsync(() => MessageBox.Avalonia.MessageBoxManager
+                    .GetMessageBoxCustomWindow(new MessageBoxCustomParams
+                    {
+                        ButtonDefinitions =
+                        [
+                            new ButtonDefinition { Name = "Продолжить" },
+                            new ButtonDefinition { Name = "Отмена", IsCancel = true }
+                        ],
+                        CanResize = true,
+                        ContentTitle = "Реорганизация",
+                        ContentMessage = "После реорганизации поля территориального обособленного подразделения очистятся",
+                        MinWidth = 300,
+                        MinHeight = 125,
+                        WindowStartupLocation = WindowStartupLocation.CenterOwner,
+                        Topmost = true,
+                    }).Show());
+
+                switch (answer)
+                {
+                    case "Продолжить":
+                        {
+                            //обработка пройдет дальше
+                            break;
+                        }
+                    case null or "Отмена":
+                        {
+                            // обработка остановится
+                            return;
+                        }
+                }
+                //очищение содержимого полей
+                Storage.Rows30[1].SubjectRF.Value = "";
+                Storage.Rows30[1].JurLico.Value = "";
+                Storage.Rows30[1].ShortJurLico.Value = "";
+                Storage.Rows30[1].JurLicoAddress.Value = "";
+                Storage.Rows30[1].JurLicoFactAddress.Value = "";
+                Storage.Rows30[1].GradeFIO.Value = "";
+                Storage.Rows30[1].Telephone.Value = "";
+                Storage.Rows30[1].Fax.Value = "";
+                Storage.Rows30[1].Email.Value = "";
+                Storage.Rows30[1].Okpo.Value = "";
+                Storage.Rows30[1].Okved.Value = "";
+                Storage.Rows30[1].Okogu.Value = "";
+                Storage.Rows30[1].Oktmo.Value = "";
+                Storage.Rows30[1].Inn.Value = "";
+                Storage.Rows30[1].Kpp.Value = "";
+                Storage.Rows30[1].Okopf.Value = "";
+                Storage.Rows30[1].Okfs.Value = "";
+                //реорганизация формы
+            }
+            form_30VM.IsSeparateDivision = !form_30VM.IsSeparateDivision;
         }
     }
 }
