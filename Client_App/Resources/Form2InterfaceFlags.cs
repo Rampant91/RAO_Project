354297bc2a26f00331440feb979d1987e50fb3f9
BalaBoBik <115734734+BalaBoBik@users.noreply.github.com>
2026-09-10 15:20:39 +0300

    Update Form2InterfaceFlags.cs

--- Client_App/Resources/Form2InterfaceFlags.cs
+++ Client_App/Resources/Form2InterfaceFlags.cs
@@ -10,7 +10,7 @@ public static class Form2InterfaceFlags
     /// <summary>В Debug можно включить для тестирования нового UI форм 2.x.</summary>
     public static bool UseNewInterface { get; set; } = true;
 #else
-    public const bool UseNewInterface = false;
+    public const bool UseNewInterface = true;
 #endif
 
     public static bool IsEnabledFor(string? formNum) =>
