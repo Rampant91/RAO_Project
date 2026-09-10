1ffe6d515428f0351bccd82aabb0a6995d6027bc
BalaBoBik <115734734+BalaBoBik@users.noreply.github.com>
2026-09-10 15:20:35 +0300

    Add Migration

---
+++ Models/DBRealization/Migrations/DataModel/20260910112906_SumRows21AndRows22.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Models.DBRealization.Migrations.DataModel
+{
+    public partial class SumRows21AndRows22 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSumRow",
+                table: "form_22",
+                type: "BOOLEAN",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsSumRow",
+                table: "form_21",
+                type: "BOOLEAN",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsSumRow",
+                table: "form_22");
+
+            migrationBuilder.DropColumn(
+                name: "IsSumRow",
+                table: "form_21");
+        }
+    }
+}
